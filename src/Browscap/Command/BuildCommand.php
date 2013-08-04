9db4a445337355e58f2da6529cc8ec12945c5e42
James Titcumb <james@asgrim.com>
2013-08-04 12:35:13 +0100

    Make build dir

--- src/Browscap/Command/BuildCommand.php
+++ src/Browscap/Command/BuildCommand.php
@@ -69,6 +69,8 @@ class BuildCommand extends Command
 
         $buildFolder = __DIR__ . '/../../../build';
 
+        mkdir($buildFolder);
+
         $generator = new BrowscapIniGenerator();
 
         file_put_contents($buildFolder . '/browscap.ini', $generator->generate($userAgents));
