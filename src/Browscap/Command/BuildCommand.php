ed3aaa47929cf00c0d616cdfab7cf6f690fae2c9
James Titcumb <james@asgrim.com>
2013-08-29 22:28:37 +0100

    Added new build script for new JSON schema

--- src/Browscap/Command/BuildCommand.php
+++ src/Browscap/Command/BuildCommand.php
@@ -5,6 +5,7 @@ namespace Browscap\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Browscap\Generator\BrowscapIniGenerator;
 
 /**
  * @author James Titcumb <james@asgrim.com>
@@ -15,6 +16,16 @@ class BuildCommand extends Command
      * @var \Symfony\Component\Console\Output\OutputInterface
      */
     protected $output;
+
+    /**
+     * @var string
+     */
+    protected $resourceFolder;
+
+    /**
+     * @var string
+     */
+    protected $buildFolder;
 
     /**
      * (non-PHPdoc)
@@ -35,5 +46,26 @@ class BuildCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->output = $output;
+        $this->resourceFolder = './resources';
+        $this->buildFolder = __DIR__ . '/../../../build';
+
+        $generator = new BrowscapIniGenerator();
+
+        $uaSourceDirectory = $this->resourceFolder . '/user-agents';
+
+        $iterator = new \RecursiveDirectoryIterator($uaSourceDirectory);
+
+        foreach (new \RecursiveIteratorIterator($iterator) as $file) {
+        	if (!$file->isFile() || $file->getExtension() != 'json') continue;
+
+        	$msg = sprintf('<info>Processing file %s ...</info>', $file->getPathname());
+        	$this->output->writeln($msg);
+        	$generator->addSourceFile($file->getPathname());
+        }
+
+        $this->output->writeln('<info>Generating browscap.ini</info>');
+        file_put_contents($this->buildFolder . '/browscapTest.ini', $generator->generateBrowscapIni());
+        $this->output->writeln('<info>All done.</info>');
     }
+
 }
---
+++ src/Browscap/Generator/BrowscapIniGenerator.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Browscap\Generator;
+
+class BrowscapIniGenerator
+{
+    protected $divisions;
+
+    protected $divisionsHaveBeenSorted = false;
+
+    /**
+     * Load a JSON file, parse it's JSON and add it to our divisions list
+     *
+     * @param string $src Name of the file
+     * @throws \Exception if the file does not exist
+     */
+    public function addSourceFile($src)
+    {
+        if (!file_exists($src)) {
+            throw new \Exception("File {$src} does not exist.");
+        }
+
+        $fileContent = file_get_contents($src);
+        $json = json_decode($fileContent, true);
+
+        $this->divisions[$json['divisionIndex']] = $json;
+    }
+
+    /**
+     * Sort the divisions (if they haven't already been sorted)
+     *
+     * @todo Change the way the sorting works. Currently it's a "unique" ID sorting
+     * but we should do this by having a general "sort order" value instead so that
+     * collisions don't matter, and if we wanted to insert a division after Default
+     * Properties for example, we'd have to renumber all the other UAs!!! carnage.
+     *
+     * @return boolean
+     */
+    public function sortDivisions()
+    {
+        if (!$this->divisionsHaveBeenSorted) {
+            if (ksort($this->divisions, SORT_NUMERIC)) {
+                $this->divisionsHaveBeenSorted = true;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function generateBrowscapIni()
+    {
+        $this->sortDivisions();
+
+        $output = $this->generateHeader();
+
+        foreach ($this->divisions as $division) {
+            if ($division['division'] == 'Browscap Version') continue;
+
+            $output .= ';;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ' . $division['division'] . "\n\n";
+
+            foreach ($division['userAgents'] as $uaData) {
+                $output .= '[' . $uaData['userAgent'] . "]\n";
+
+                foreach ($uaData['properties'] as $property => $value) {
+                    $output .= sprintf("%s=%s\n", $property, $value);
+                }
+
+                $output .= "\n";
+            }
+        }
+
+        return $output;
+    }
+
+    public function generateHeader($version = '5021')
+    {
+        $dateUtc = date('l, F j, Y \a\t h:i A T');
+        $date = date('r');
+
+        $header = "";
+
+        $header .= ";;; Provided courtesy of http://tempdownloads.browserscap.com/\n";
+        $header .= ";;; Created on {$dateUtc}\n\n";
+
+        $header .= ";;; Keep up with the latest goings-on with the project:\n";
+        $header .= ";;; Follow us on Twitter <https://twitter.com/browscap>, or...\n";
+        $header .= ";;; Like us on Facebook <https://facebook.com/browscap>, or...\n";
+        $header .= ";;; Collaborate on GitHub <https://github.com/GaryKeith/browscap>, or...\n";
+        $header .= ";;; Discuss on Google Groups <https://groups.google.com/d/forum/browscap>.\n\n";
+
+        $header .= ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Browscap Version\n\n";
+
+        $header .= "[GJK_Browscap_Version]\n";
+        $header .= "Version={$version}\n";
+        $header .= "Released={$date}\n\n";
+
+        return $header;
+    }
+}
