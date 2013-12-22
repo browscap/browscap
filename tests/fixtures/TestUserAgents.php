9e8d22ec3cff7a777961e64a2c6cdfab3f6034a9
James Titcumb <james@asgrim.com>
2013-12-22 22:45:17 +0000

    Added test for #48

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -1,6 +1,15 @@
 <?php
 
 return [
+    'issue-48' => [
+        'Mozilla/5.0 (Android; Mobile; rv:26.0) Gecko/26.0 Firefox/26.0',
+        [
+            'Browser' => 'Firefox',
+            'Version' => '26.0',
+            'Platform' => 'Android',
+            'isMobileDevice' => true,
+        ],
+    ],
     'chrome31' => [
         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36',
         [
