4fe60c59349d1eda225022b37f0a3cd10a181002
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-01-02 23:52:23 +0100

    added unittests for issue 47 of phpbrowscap

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -1,6 +1,78 @@
 <?php
 
 return [
+    'issue-47-phpbrowscap-8' => [
+        'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36',
+        [
+            'Browser' => 'Chrome',
+            'Version' => '31.0',
+            'Platform' => 'WinXP',
+            'isMobileDevice' => false,
+        ],
+    ],
+    'issue-47-phpbrowscap-7' => [
+        'Mozilla/5.0 (Windows NT 5.1; rv:26.0) Gecko/20100101 Firefox/26.0',
+        [
+            'Browser' => 'Firefox',
+            'Version' => '26.0',
+            'Platform' => 'WinXP',
+            'isMobileDevice' => false,
+        ],
+    ],
+    'issue-47-phpbrowscap-6' => [
+        'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36 OPR/18.0.1284.49',
+        [
+            'Browser' => 'Opera',
+            'Version' => '18.0',
+            'Platform' => 'Win8.1',
+            'isMobileDevice' => false,
+        ],
+    ],
+    'issue-47-phpbrowscap-5' => [
+        'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        [
+            'Browser' => 'MSIE',
+            'Version' => '11.0',
+            'Platform' => 'Win8.1',
+            'isMobileDevice' => false,
+        ],
+    ],
+    'issue-47-phpbrowscap-4' => [
+        'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36',
+        [
+            'Browser' => 'Chrome',
+            'Version' => '31.0',
+            'Platform' => 'Win8.1',
+            'isMobileDevice' => false,
+        ],
+    ],
+    'issue-47-phpbrowscap-3' => [
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        [
+            'Browser' => 'MSIE',
+            'Version' => '11.0',
+            'Platform' => 'Win7',
+            'isMobileDevice' => false,
+        ],
+    ],
+    'issue-47-phpbrowscap-2' => [
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0',
+        [
+            'Browser' => 'Firefox',
+            'Version' => '25.0',
+            'Platform' => 'Win7',
+            'isMobileDevice' => false,
+        ],
+    ],
+    'issue-47-phpbrowscap-1' => [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:25.0) Gecko/20100101 Firefox/25.0 FirePHP/0.7.4',
+        [
+            'Browser' => 'Firefox',
+            'Version' => '25.0',
+            'Platform' => 'MacOSX',
+            'isMobileDevice' => false,
+        ],
+    ],
     'issue-7' => [
         'Mozilla/5.0 (Linux; Android 4.2.2; Nexus 4 Build/JDQ39E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.90 Mobile Safari/537.36',
         [
@@ -25,6 +97,7 @@ return [
         [
             'Browser' => 'Chrome',
             'Version' => '31.0',
+            'Platform' => 'Linux',
         ],
     ],
     'issue-46-a' => [
