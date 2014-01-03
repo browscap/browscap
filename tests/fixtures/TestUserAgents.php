dc77c4bba53673472fa4fe7c48c7cab867c5d30c
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-01-03 17:37:38 +0100

    added new Browser Midori from Issue 50

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -1,6 +1,24 @@
 <?php
 
 return [
+    'issue-50-2' => [
+        'Mozilla/5.0 (X11; Linux) AppleWebKit/531.2+ Midori/0.3',
+        [
+            'Browser' => 'Midori',
+            'Version' => '0.3',
+            'Platform' => 'Linux',
+            'isMobileDevice' => true,
+        ],
+    ],
+    'issue-50-1' => [
+        'Mozilla/5.0 (X11; Linux) AppleWebKit/537.32 (KHTML, like Gecko) Chrome/18.0.1025.133 Safari/537.32 Midori/0.5',
+        [
+            'Browser' => 'Midori',
+            'Version' => '0.5',
+            'Platform' => 'Linux',
+            'isMobileDevice' => true,
+        ],
+    ],
     'issue-51-3' => [
         'Mozilla/5.0 (compatible; grapeFX/0.9; crawler@grapeshot.co.uk',
         [
