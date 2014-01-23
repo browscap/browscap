cec51845cacb54ff2442b9ed6aae60e5542f0f0c
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-01-23 22:58:21 +0100

    added test

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -1,6 +1,16 @@
 <?php
 
 return [
+    'issue-mobiles-F' => [
+        'Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; GT-I9100G Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) FlyFlow/2.5 Version/4.0 Mobile Safari/533.1 baidubrowser/042_6.3.5.2_diordna_008_084/gnusmas_01_4.3.2_G0019I-TG/7400001l/AFCD145CE647EC590CFE42154CB19B89%7C274573340474753/1',
+        [
+            'Browser' => 'FlyFlow',
+            'Version' => '2.5',
+            'Platform' => 'Android',
+            'Platform_Version' => '2.3',
+            'isMobileDevice' => true,
+        ],
+    ],
     'issue-mobiles-E' => [
         'Dalvik/1.6.0 (Linux; U; Android 4.0.4; GT-I9100 Build/IMM76D)',
         [
