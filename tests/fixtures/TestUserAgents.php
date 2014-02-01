c1a73918eb5d7bd6455551d9bd0c170856648c8a
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-02-01 22:57:25 +0100

    added unit test

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -1,6 +1,16 @@
 <?php
 
 return [
+    'issue-102' => [
+        'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.16) Gecko/20110322 Fedora/3.6.16-1.fc14 Firefox/3.6.16',
+        [
+            'Browser' => 'Firefox',
+            'Version' => '3.6',
+            'Platform' => 'Linux',
+            'isMobileDevice' => false,
+            'Crawler' => false,
+        ],
+    ],
     'issue-99-C' => [
         'DavClnt',
         [
