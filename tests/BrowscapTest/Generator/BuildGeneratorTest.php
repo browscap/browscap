0a89e804e090d3baa5d1140c0d4e51e49cecbacc
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-02-09 23:04:32 +0100

    added new unit test

---
+++ tests/BrowscapTest/Generator/BuildGeneratorTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace BrowscapTest\Generator;
+
+use Browscap\Generator\BuildGenerator;
+class BuildGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array
+     */
+    private $messages = array();
+    
+    public function setUp()
+    {
+        $this->messages = array();
+    }
+    
+    public function mockLog($level, $message)
+    {
+        $this->messages[] = array(
+            'level' => $level,
+            'message' => $message
+        );
+    }
+    
+    public function testConstructFailsWithoutParameters()
+    {
+        $this->setExpectedException('\Exception', 'You must specify a resource folder');
+        new BuildGenerator(null, null);
+    }
+    
+    public function testConstructFailsWithoutTheSecondParameter()
+    {
+        $this->setExpectedException('\Exception', 'You must specify a build folder');
+        new BuildGenerator('.', null);
+    }
+    
+    public function testConstructFailsIfTheDirDoesNotExsist()
+    {
+        $this->setExpectedException('\Exception', 'The directory \'/dar\' does not exist, or we cannot access it');
+        new BuildGenerator('/dar', null);
+    }
+    
+    public function testConstructFailsIfTheDirIsNotAnDirectory()
+    {
+        $this->setExpectedException('\Exception', 'The path \'' . __FILE__ . '\' did not resolve to a directory');
+        new BuildGenerator(__FILE__, null);
+    }
+    
+    public function testConstructPassesIfAllDirsExist()
+    {
+        new BuildGenerator('.', '.');
+    }
+    
+    public function testSetLogger()
+    {
+        $mock = $this->getMock('\\Monolog\\Logger', array(), array(), '', false);
+        
+        $generator = new BuildGenerator('.', '.');
+        self::assertSame($generator, $generator->setLogger($mock));
+    }
+    
+    public function testBuild()
+    {
+        $mock = $this->getMock('\\Monolog\\Logger', array('log'), array(), '', false);
+        $mock->expects($this->any())
+            ->method('log')
+            ->will(self::returnCallback(array($this, 'mockLog')))
+        ;
+        
+        $generator = new BuildGenerator('.', '.');
+        self::assertSame($generator, $generator->setLogger($mock));
+        
+        $generator->generateBuilds('test');
+    }
+}
