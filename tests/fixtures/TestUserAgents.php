59f5c231ad249dd4701fa3cfc1b26e1790a7c88b
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-02-15 00:07:17 +0100

    added rule and unit test

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -1,6 +1,15 @@
 <?php
 
 return [
+    'issue-122' => [
+        'Mozilla/5.0 (Windows NT 6.1; rv:6.0) Gecko/20110814 Firefox/6.0 Google (+https://developers.google.com/+/web/snippet/)',
+        [
+            'Browser' => 'Google Web Snippet',
+            'Platform' => 'Win7',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+        ],
+    ],
     'issue-117' => [
         'netEstate NE Crawler (+http://www.website-datenbank.de/)',
         [
