870356ddb725348dfd51da984b81622caeb8261c
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-02-15 15:04:04 +0100

    added rule and test

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -1,6 +1,15 @@
 <?php
 
 return [
+    'issue-128' => [
+        'Mozilla/5.0 (compatible; Another Web Mining Tool 1.0; +none; awmt)',
+        [
+            'Browser' => 'Another Web Mining Tool',
+            'Version' => '1.0',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+        ],
+    ],
     'issue-120' => [
         'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0_1 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko; Google Page Speed Insights) Version/6.0 Mobile/10A525 Safari/8536.25',
         [
