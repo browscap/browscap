4353eb8d7cae44f060ff8bc2cbacbcef82da13d5
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-02-22 11:51:53 +0100

    added new Version of Konqueror and test

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -1,6 +1,15 @@
 <?php
 
 return [
+    'issue-137' => [
+        'Mozilla/5.0 (compatible; Konqueror/2.2.2; Linux 2.4.14-xfs; X11; i686)',
+        [
+            'Browser' => 'Konqueror',
+            'Version' => '2.2',
+            'isMobileDevice' => false,
+            'Crawler' => false,
+        ],
+    ],
     'issue-135' => [
         'Mozilla/5.0 (compatible; WBSearchBot/1.1; +http://www.warebay.com/bot.html)',
         [
