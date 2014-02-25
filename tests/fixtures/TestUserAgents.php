9a32966c4a8b3b561f370a8cb6e2de1da6638744
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-02-25 23:00:14 +0100

    fixed test

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -5,7 +5,6 @@ return [
         'Nokia7230/5.0 (05.71) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420+',
         [
             'Browser' => 'Nokia',
-            'Version' => '3.9',
             'Platform' => 'JAVA',
             'isMobileDevice' => true,
             'Device_Name' => '7230',
