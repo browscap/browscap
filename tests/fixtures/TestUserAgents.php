ab92bd04f9c72879d9e52fd5e88edb2ac6c38d4e
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-02-26 23:50:21 +0100

    added test

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -1,6 +1,14 @@
 <?php
 
 return [
+    'issue-147' => [
+        'Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp',
+        [
+            'Browser' => 'Yahoo! Slurp',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+        ],
+    ],
     'issue-141' => [
         'Mozilla/5.0 TYPO3-linkvalidator',
         [
