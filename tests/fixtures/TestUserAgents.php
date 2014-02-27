252a7a458e6f87c1e907398ec2510d45fae7327e
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-02-27 19:41:02 +0100

    added rule and test

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -1,6 +1,15 @@
 <?php
 
 return [
+    'issue-146' => [
+        'Mozilla/4.0 (compatible; MSIE 7.0;Windows NT 5.1;.NET CLR 1.1.4322;.NET CLR 2.0.50727;.NET CLR 3.0.04506.30) Lightspeedsystems',
+        [
+            'Browser' => 'Lightspeedsystems',
+            'Platform' => 'WinXP',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+        ],
+    ],
     'issue-141' => [
         'Mozilla/5.0 TYPO3-linkvalidator',
         [
