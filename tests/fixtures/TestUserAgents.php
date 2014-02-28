132afd15551c7d4fe517cb448dc8e57794416141
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-03-01 00:16:03 +0100

    added new version of Firefox and related Browsers

--- tests/fixtures/TestUserAgents.php
+++ tests/fixtures/TestUserAgents.php
@@ -1,6 +1,17 @@
 <?php
 
 return [
+    'issue-158' => [
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:30.0) Gecko/20100101 Firefox/30.0',
+        [
+            'Browser' => 'Firefox',
+            'Version' => '30.0',
+            'Platform' => 'Win7',
+            'Win64' => true,
+            'isMobileDevice' => false,
+            'Crawler' => false,
+        ],
+    ],
     'issue-141' => [
         'Mozilla/5.0 TYPO3-linkvalidator',
         [
