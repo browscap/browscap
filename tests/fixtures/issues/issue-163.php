4ae0992cbaf7c3c3259b301cef380724de691505
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-03-04 20:19:53 +0100

    added rules and tests

---
+++ tests/fixtures/issues/issue-163.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'issue-163' => [
+        'Validator.nu/LV http://validator.w3.org/services',
+        [
+            'Browser' => 'Validator.nu/LV',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+        ],
+    ],
+];
