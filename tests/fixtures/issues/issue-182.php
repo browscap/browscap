076e516db559debf1281989984319145a7ef2d1f
James Titcumb <james@asgrim.com>
2014-03-27 16:13:07 +0000

    Added Opera 20

---
+++ tests/fixtures/issues/issue-182.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'issue-182' => [
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36 OPR/20.0.1387.82',
+        [
+            'Browser' => 'Opera',
+            'Version' => '20.0',
+            'Platform' => 'Win7',
+            'Win64' => true,
+            'isMobileDevice' => false,
+        ],
+    ],
+];
