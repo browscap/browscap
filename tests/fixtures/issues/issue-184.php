28a45b03031f6ec8a8b0adc49d550f32121e0d52
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-03-29 20:56:51 +0100

    fix test

--- tests/fixtures/issues/issue-184.php
+++ tests/fixtures/issues/issue-184.php
@@ -13,7 +13,7 @@ return [
         ],
     ],
     'issue-184-B' => [
-        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; GTB6.4; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; chromeframe; Avant Browser; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; InfoPath.1; .NET CLR 3.0.4506.',
+        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; GTB5; Avant Browser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
         [
             'Browser' => 'Avant',
             'Platform' => 'WinXP',
