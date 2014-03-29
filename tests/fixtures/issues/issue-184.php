915858b7fd9070143f81c9dd2193f7d8af2c680d
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-03-29 20:51:11 +0100

    fix tests

--- tests/fixtures/issues/issue-184.php
+++ tests/fixtures/issues/issue-184.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'issue-184' => [
+    'issue-184-A' => [
         'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; Avant Browser; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)',
         [
             'Browser' => 'Avant',
@@ -11,6 +11,8 @@ return [
             'Device_Type' => 'Desktop',
             'Device_Pointing_Method' => 'mouse',
         ],
+    ],
+    'issue-184-B' => [
         'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; GTB6.4; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; chromeframe; Avant Browser; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; InfoPath.1; .NET CLR 3.0.4506.',
         [
             'Browser' => 'Avant',
@@ -20,6 +22,8 @@ return [
             'Device_Type' => 'Desktop',
             'Device_Pointing_Method' => 'mouse',
         ],
+    ],
+    'issue-184-C' => [
         'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Trident/4.0; Avant Browser; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2)',
         [
             'Browser' => 'Avant',
@@ -29,6 +33,8 @@ return [
             'Device_Type' => 'Desktop',
             'Device_Pointing_Method' => 'mouse',
         ],
+    ],
+    'issue-184-D' => [
         'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Avant Browser; Avant Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506; Tablet PC 2.0)',
         [
             'Browser' => 'Avant',
@@ -38,6 +44,8 @@ return [
             'Device_Type' => 'Desktop',
             'Device_Pointing_Method' => 'mouse',
         ],
+    ],
+    'issue-184-E' => [
         'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; FunWebProducts; Avant Browser; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)',
         [
             'Browser' => 'Avant',
@@ -47,6 +55,8 @@ return [
             'Device_Type' => 'Desktop',
             'Device_Pointing_Method' => 'mouse',
         ],
+    ],
+    'issue-184-F' => [
         'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 3.1; .NET CLR 2.0.50727)',
         [
             'Browser' => 'Avant',
@@ -56,6 +66,8 @@ return [
             'Device_Type' => 'Desktop',
             'Device_Pointing_Method' => 'mouse',
         ],
+    ],
+    'issue-184-G' => [
         'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; Avant Browser; Avant Browser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
         [
             'Browser' => 'Avant',
@@ -65,6 +77,8 @@ return [
             'Device_Type' => 'Desktop',
             'Device_Pointing_Method' => 'mouse',
         ],
+    ],
+    'issue-184-H' => [
         'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; Avant Browser; SV1; Avant Browser; .NET CLR 1.1.4322; InfoPath.1)',
         [
             'Browser' => 'Avant',
