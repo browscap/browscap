a62769697515aa2893a6cb5cf9473049845acdde
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-04-14 21:48:53 +0200

    added rule and test

---
+++ tests/fixtures/issues/issue-204.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'issue-204' => [
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Thunderbird/24.4.0 Lightning/2.6.4',
+        [
+            'Browser' => 'Thunderbird',
+            'Version' => '24.4',
+            'Platform' => 'Win7',
+            'Platform_Version' => '6.1',
+            'isMobileDevice' => false,
+            'Device_Type' => 'Desktop',
+            'Device_Pointing_Method' => 'mouse',
+        ],
+    ],
+];
