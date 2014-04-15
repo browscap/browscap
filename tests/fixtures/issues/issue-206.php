dc004b5c3f3843dacdf7800e9206019596b80ebf
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-04-15 21:54:40 +0200

    added rule and test

---
+++ tests/fixtures/issues/issue-206.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'issue-206' => [
+        'Mozilla/5.0 (compatible; DotBot/1.1; http://www.opensiteexplorer.org/dotbot, help@moz.com)',
+        [
+            'Browser' => 'DotBot',
+            'Version' => '1.1',
+            'Win64' => false,
+            'isMobileDevice' => false,
+            'Crawler' => true
+        ],
+    ],
+];
