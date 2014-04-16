72cfba21f519ae9a959f4057a1215aaa90c08b58
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-04-16 23:04:34 +0200

    #issue 213: added rule and test

---
+++ tests/fixtures/issues/issue-213.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'issue-213' => [
+        'Mozilla/5.0 (Linux; U; Android 4.3; fr-fr; GT-I9305 Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+        [
+            'Browser' => 'Android',
+            'Version' => '4.0',
+            'Platform' => 'Android',
+            'Platform_Version' => '4.3',
+            'isMobileDevice' => true,
+            'Crawler' => false,
+            'Device_Name' => 'Galaxy S III LTE International',
+            'Device_Maker' => 'Samsung',
+            'Device_Type' => 'Mobile Phone',
+            'Device_Pointing_Method' => 'touchscreen',
+        ],
+    ],
+];
