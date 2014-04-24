9c81518682034128ca51307226873d2a29d66a1a
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-04-24 21:33:55 +0200

    Issue 224: added rule and test

---
+++ tests/fixtures/issues/issue-224.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'issue-224-A' => [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 1084) AppleWebKit/536.29.13 (KHTML like Gecko) Version/6.0.4 Safari/536.29.13',
+        [
+            'Browser' => 'Fake Safari',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+        ],
+    ],
+    'issue-224-B' => [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 107) AppleWebKit/534.48.3 (KHTML like Gecko) Version/5.1 Safari/534.48.3',
+        [
+            'Browser' => 'Fake Safari',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+        ],
+    ],
+];
