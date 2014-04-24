55b1a39d71a3bee0dd38e592e9d253bd39b5e80d
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-04-24 22:36:11 +0200

    Issue 227: added rule and test, grouped general crawlers into one folder, reformated files

---
+++ tests/fixtures/issues/issue-227.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'issue-227' => [
+        'securepoint cf',
+        [
+            'Browser' => 'RankFlex.com Webspider',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+            'Browser_Maker' => 'rankflex.com'
+        ],
+    ],
+];
