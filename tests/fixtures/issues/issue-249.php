bcf49152445b6ea4048dda4313aa139e89107bbf
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-04-30 21:50:25 +0200

    add rule and test, reformat json file

---
+++ tests/fixtures/issues/issue-249.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'issue-249' => [
+        'Mozilla/5.0 (FHScan Core 1.1)',
+        [
+            'Browser' => 'FHScan Core',
+            'Version' => '1.1',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+            'Browser_Maker' => 'tarasco.org',
+        ],
+    ],
+];
