c290035b79f1382801e05cb8461c6229a97c97ac
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-05-06 23:00:46 +0200

    add rule and test

---
+++ tests/fixtures/issues/issue-258.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'issue-258' => [
+        'Mozilla/5.0 (compatible; nbot/2.0)',
+        [
+            'Browser' => 'nbot',
+            'Version' => '2.0',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+        ],
+    ],
+];
