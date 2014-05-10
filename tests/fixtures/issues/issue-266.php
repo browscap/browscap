f9ab3e1907cf428e3c76232700b5206d7f7fae95
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-05-10 18:38:46 +0200

    add test

---
+++ tests/fixtures/issues/issue-266.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'issue-266' => [
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:32.0) Gecko/20100101 Firefox/32.0',
+        [
+            'Browser' => 'Firefox',
+            'Browser_Maker' => 'Mozilla Foundation',
+            'Browser_Bits' => '32',
+            'Platform' => 'Linux',
+            'Platform_Bits' => '32',
+            'Version' => '32.0',
+            'isMobileDevice' => false,
+            'Crawler' => false,
+            'Device_Type' => 'Desktop',
+            'Device_Name' => 'Linux Desktop',
+            'Device_Pointing_Method' => 'mouse',
+        ],
+    ],
+];
