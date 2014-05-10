4fc959bc3bb077741f315e54531147c484dac0c1
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-05-10 20:00:47 +0200

    add rule and test

---
+++ tests/fixtures/issues/issue-269.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'issue-269-A' => [
+        'Slackbot-LinkExpanding (+https://api.slack.com/robots)',
+        [
+            'Browser' => 'Slackbot-Link-Expanding',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+            'Browser_Maker' => 'slack.com',
+        ],
+    ],
+    'issue-269-B' => [
+        'Slackbot (+https://api.slack.com/robots)',
+        [
+            'Browser' => 'Slackbot',
+            'isMobileDevice' => false,
+            'Crawler' => true,
+            'Browser_Maker' => 'slack.com',
+        ],
+    ],
+];
