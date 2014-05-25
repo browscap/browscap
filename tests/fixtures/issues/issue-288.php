0d42c45b8ac6f951424bbb884658450d8c8a4349
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-05-25 20:26:17 +0200

    add test

---
+++ tests/fixtures/issues/issue-288.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'issue-288' => [
+        'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 7 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.122 Safari/537.36',
+        [
+            'Browser' => 'Chrome',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Google Inc',
+            'Version' => '35.0',
+            'MajorVer' => '35',
+            'MinorVer' => '0',
+            'Platform' => 'Android',
+            'Platform_Version' => '4.4',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Google Inc',
+            'isMobileDevice' => true,
+            'isTablet' => true,
+            'Crawler' => false,
+            'Device_Name' => 'Nexus 7',
+            'Device_Maker' => 'Asus',
+            'Device_Type' => 'Tablet',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'Nexus 7',
+            'Device_Brand_Name' => 'Google',
+            'RenderingEngine_Name' => 'Blink',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Google Inc',
+        ],
+    ],
+];
