4b7ca9ce2adca0d4d6e6ed0f4bc06870f45f6f03
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-06-07 11:35:48 +0200

    add test

---
+++ tests/fixtures/issues/issue-305.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'issue-305' => [
+        'Microsoft Office Protocol Discovery',
+        [
+            'Browser' => 'MS OPD',
+            'Browser_Type' => 'Bot/Crawler',
+            'Browser_Bits' => '0',
+            'Browser_Maker' => 'Microsoft Corporation',
+            'Version' => '0.0',
+            'MajorVer' => '0',
+            'MinorVer' => '0',
+            'Platform' => 'unknown',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '0',
+            'Platform_Maker' => 'unknown',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => true,
+            'Device_Name' => 'unknown',
+            'Device_Maker' => 'unknown',
+            'Device_Type' => 'unknown',
+            'Device_Pointing_Method' => 'unknown',
+            'Device_Code_Name' => 'unknown',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'unknown',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'unknown',
+        ],
+    ],
+];
