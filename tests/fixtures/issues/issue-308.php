f0b4140659ac60fd9ae9209c5aafa4f49ffeaf6f
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-06-11 21:23:05 +0200

    add rule and test

---
+++ tests/fixtures/issues/issue-308.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'issue-308' => [
+        'Microsoft Office Excel 2013',
+        [
+            'Browser' => 'Excel',
+            'Browser_Type' => 'Application',
+            'Browser_Bits' => '0',
+            'Browser_Maker' => 'Microsoft Corporation',
+            'Version' => '2013',
+            'MajorVer' => '2013',
+            'MinorVer' => '0',
+            'Platform' => 'unknown',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '0',
+            'Platform_Maker' => 'unknown',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => true,
+            'Device_Name' => 'unknown',
+            'Device_Maker' => 'unknown',
+            'Device_Type' => 'Desktop',
+            'Device_Pointing_Method' => 'unknown',
+            'Device_Code_Name' => 'unknown',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'Trident',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Microsoft Corporation',
+        ],
+    ],
+];
