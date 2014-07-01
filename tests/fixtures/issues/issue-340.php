97661a8439b85b18d8f35bba890ef31b6f0179ea
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-07-01 22:20:54 +0200

    add new rules, a test and a new android

---
+++ tests/fixtures/issues/issue-340.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'issue-340' => [
+        'Mozilla/5.0 (Android; U; -GB) AppleWebKit/533.19.4 (KHTML, like Gecko) AdobeAIR/13.0',
+        [
+            'Browser' => 'Adobe AIR',
+            'Browser_Type' => 'Application',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Adobe Systems Incorporated',
+            'Version' => '13.0',
+            'MajorVer' => '13',
+            'MinorVer' => '0',
+            'Platform' => 'Android',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Google Inc',
+            'isMobileDevice' => true,
+            'isTablet' => false,
+            'Crawler' => false,
+            'Device_Name' => 'general Mobile Device',
+            'Device_Maker' => 'unknown',
+            'Device_Type' => 'Mobile Phone',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'general Mobile Device',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'WebKit',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Apple Inc',
+        ],
+    ],
+];
