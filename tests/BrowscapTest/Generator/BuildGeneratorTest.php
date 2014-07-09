218dabe35faec5191a18c6e0533af708ffe5275e
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-07-09 07:32:51 +0200

    add new tests

--- tests/BrowscapTest/Generator/BuildGeneratorTest.php
+++ tests/BrowscapTest/Generator/BuildGeneratorTest.php
@@ -48,7 +48,7 @@ class BuildGeneratorTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->markTestSkipped('need to be updated');
-
+        
         $this->logger   = new Logger('browscapTest', array(new NullHandler()));
         $this->messages = array();
     }
@@ -106,22 +106,6 @@ class BuildGeneratorTest extends \PHPUnit_Framework_TestCase
         self::assertSame($generator, $generator->setCollectionCreator($mock));
     }
 
-    public function testSetCollectionParser()
-    {
-        $mock = $this->getMock('\\Browscap\\Generator\\CollectionParser', array(), array(), '', false);
-
-        $generator = new BuildGenerator('.', '.');
-        self::assertSame($generator, $generator->setCollectionParser($mock));
-    }
-
-    public function testSetGeneratorHelper()
-    {
-        $mock = $this->getMock('\\Browscap\\Helper\\Generator', array(), array(), '', false);
-
-        $generator = new BuildGenerator('.', '.');
-        self::assertSame($generator, $generator->setGeneratorHelper($mock));
-    }
-
     public function testBuild()
     {
         $mockCollection = $this->getMock('\\Browscap\\Generator\\DataCollection', array('getGenerationDate'), array(), '', false);
@@ -136,38 +120,12 @@ class BuildGeneratorTest extends \PHPUnit_Framework_TestCase
             ->will(self::returnValue($mockCollection))
         ;
 
-        $mockGenerator = $this->getMock('\\Browscap\\Helper\\Generator', array('setVersion', 'create'), array(), '', false);
-        $mockGenerator->expects(self::any())
-            ->method('setVersion')
-            ->will(self::returnSelf())
-        ;
-        $mockGenerator->expects(self::any())
-            ->method('create')
-            ->will(self::returnValue('This is a test'))
-        ;
-
-        $mockParser = $this->getMock('\\Browscap\\Generator\\CollectionParser', array('parse', 'setLogger', 'getLogger'), array(), '', false);
-        $mockParser->expects(self::any())
-            ->method('parse')
-            ->will(self::returnValue(array()))
-        ;
-        $mockParser->expects(self::any())
-            ->method('setLogger')
-            ->will(self::returnSelf())
-        ;
-        $mockParser->expects(self::any())
-            ->method('getLogger')
-            ->will(self::returnValue($this->logger))
-        ;
-
         $buildDir = sys_get_temp_dir() . '/bcap-build-generator-test/';
         mkdir($buildDir);
 
         $generator = new BuildGenerator('.', $buildDir);
         self::assertSame($generator, $generator->setLogger($this->logger));
         self::assertSame($generator, $generator->setCollectionCreator($mockCreator));
-        self::assertSame($generator, $generator->setCollectionParser($mockParser));
-        self::assertSame($generator, $generator->setGeneratorHelper($mockGenerator));
 
         $generator->run('test');
     }
---
+++ tests/BrowscapTest/Writer/CsvWriterTest.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Copyright (c) 1998-2014 Browser Capabilities Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Refer to the LICENSE file distributed with this package.
+ *
+ * @category   BrowscapTest
+ * @package    Writer
+ * @copyright  1998-2014 Browser Capabilities Project
+ * @license    MIT
+ */
+
+namespace BrowscapTest\Writer;
+
+use Browscap\Writer\CsvWriter;
+use Monolog\Handler\NullHandler;
+use Monolog\Logger;
+use org\bovigo\vfs\vfsStream;
+
+/**
+ * Class CsvWriterTest
+ *
+ * @category   BrowscapTest
+ * @package    Writer
+ * @author     Thomas Müller <t_mueller_stolzenhain@yahoo.de>
+ */
+class CsvWriterTest extends \PHPUnit_Framework_TestCase
+{
+    const STORAGE_DIR = 'storage';
+
+    /**
+     * @var \Browscap\Writer\CsvWriter
+     */
+    private $object = null;
+
+    /**
+     * @var \org\bovigo\vfs\vfsStreamDirectory
+     */
+    private $root = null;
+
+    /**
+     * @var \org\bovigo\vfs\vfsStreamDirectory
+     */
+    private $file = null;
+
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     *
+     */
+    public function setUp()
+    {
+        $this->root = vfsStream::setup(self::STORAGE_DIR);
+        $this->file = vfsStream::url(self::STORAGE_DIR) . DIRECTORY_SEPARATOR . 'test.csv';
+        
+        $this->object = new CsvWriter($this->file);
+    }
+
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     *
+     */
+    public function teardown()
+    {
+        $this->object->close();
+        
+        unlink($this->file);
+    }
+
+    public function testSetGetLogger()
+    {
+        $mockLogger = $this->getMock('\Monolog\Logger', array(), array(), '', false);
+
+        self::assertSame($this->object, $this->object->setLogger($mockLogger));
+        self::assertSame($mockLogger, $this->object->getLogger());
+    }
+
+    public function testSetGetFormatter()
+    {
+        $mockFormatter = $this->getMock('\Browscap\Formatter\CsvFormatter', array(), array(), '', false);
+
+        self::assertSame($this->object, $this->object->setFormatter($mockFormatter));
+        self::assertSame($mockFormatter, $this->object->getFormatter());
+    }
+
+    public function testSetGetFilter()
+    {
+        $mockFilter = $this->getMock('\Browscap\Filter\FullFilter', array(), array(), '', false);
+
+        self::assertSame($this->object, $this->object->setFilter($mockFilter));
+        self::assertSame($mockFilter, $this->object->getFilter());
+    }
+
+    public function testSetGetSilent()
+    {
+        $silent = true;
+
+        self::assertSame($this->object, $this->object->setSilent($silent));
+        self::assertSame($silent, $this->object->isSilent());
+    }
+
+    public function testFileStart()
+    {
+        $silent = true;
+
+        self::assertSame($this->object, $this->object->fileStart());
+        self::assertSame('', file_get_contents($this->file));
+    }
+
+    public function testFileEnd()
+    {
+        $silent = true;
+
+        self::assertSame($this->object, $this->object->fileEnd());
+        self::assertSame('', file_get_contents($this->file));
+    }
+}
