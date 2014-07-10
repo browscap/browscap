28ba4e1e2a696eec40ed4d0949d71b29105acf2b
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2014-07-10 23:01:43 +0200

    fix tests for ini writer

--- tests/BrowscapTest/Writer/IniWriterTest.php
+++ tests/BrowscapTest/Writer/IniWriterTest.php
@@ -318,7 +318,7 @@ class IniWriterTest extends \PHPUnit_Framework_TestCase
         self::assertSame($this->object, $this->object->setFilter($mockFilter));
 
         self::assertSame($this->object, $this->object->renderSectionBody($section, $mockCollection));
-        self::assertSame('Test="1"' . PHP_EOL, file_get_contents($this->file));
+        self::assertSame('Comment="1"' . PHP_EOL, file_get_contents($this->file));
     }
 
     public function testRenderSectionBodyIfSilent()
