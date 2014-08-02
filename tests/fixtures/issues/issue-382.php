f7aaea58b2f664b754fbc19a69c3cb921b665fd6
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-08-02 23:20:56 +0200

    added rules and test

---
+++ tests/fixtures/issues/issue-382.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'issue-382' => [
+        'iOS/6.1.3 (10B329) dataaccessd/1.0',
+        [
+            'Browser' => 'iOS dataaccessd',
+            'Browser_Type' => 'Application',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Apple Inc',
+            'Version' => '1.0',
+            'MajorVer' => '1',
+            'MinorVer' => '0',
+            'Platform' => 'iOS',
+            'Platform_Version' => '6.1',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Apple Inc',
+            'Win64' => false,
+            'isMobileDevice' => true,
+            'isTablet' => false,
+            'Crawler' => false,
+            'Device_Name' => 'general Mobile Device',
+            'Device_Maker' => 'Apple Inc',
+            'Device_Type' => 'Mobile Device',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'general Mobile Device',
+            'Device_Brand_Name' => 'Apple',
+            'RenderingEngine_Name' => 'WebKit',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Apple Inc',
+        ],
+    ],
+];
