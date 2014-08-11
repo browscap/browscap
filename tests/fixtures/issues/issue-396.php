7725d81d61030307aa40964111efe379436c1ffa
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-08-11 20:31:29 +0200

    add new rules and tests, sort browsers

---
+++ tests/fixtures/issues/issue-396.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'issue-396' => [
+        'Mozilla/5.0 (Linux; U; Android 2.3.6; fa-fa; GT-S5830i Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1 MxBrowser/4.3.0.2000',
+        [
+            'Browser' => 'Maxthon',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Maxthon International Limited',
+            'Version' => '4.0',
+            'MajorVer' => '4',
+            'MinorVer' => '0',
+            'Platform' => 'Android',
+            'Platform_Version' => '2.3',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Google Inc',
+            'isMobileDevice' => true,
+            'isTablet' => false,
+            'Crawler' => false,
+            'Device_Name' => 'Galaxy Ace',
+            'Device_Maker' => 'Samsung',
+            'Device_Type' => 'Mobile Phone',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'GT-S5830i',
+            'Device_Brand_Name' => 'Samsung',
+            'RenderingEngine_Name' => 'WebKit',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Apple Inc',
+        ],
+    ],
+];
