3e28942760ac62ae54bc281b51de11e72fe82ae7
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-08-16 21:44:11 +0200

    change device name

--- tests/fixtures/issues/issue-398.php
+++ tests/fixtures/issues/issue-398.php
@@ -18,7 +18,7 @@ return [
             'isMobileDevice' => true,
             'isTablet' => false,
             'Crawler' => false,
-            'Device_Name' => 'Galaxy Note 3 LTE 32GB',
+            'Device_Name' => 'Galaxy Note 3 LTE',
             'Device_Maker' => 'Samsung',
             'Device_Type' => 'Mobile Phone',
             'Device_Pointing_Method' => 'touchscreen',
