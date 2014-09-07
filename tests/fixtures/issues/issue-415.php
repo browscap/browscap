8e77685aba5ad5e8bfc8e39c5c03538568e6bdcb
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-09-07 12:39:42 +0200

    add new rules and test

---
+++ tests/fixtures/issues/issue-415.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'issue-415' => [
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; en; rv:1.9.0.13) Gecko/2009073022 Firefox/3.5.2 (.NET CLR 3.5.30729) SurveyBot/2.3 (DomainTools)',
+        [
+            'Browser' => 'SurveyBot',
+            'Browser_Type' => 'Bot/Crawler',
+            'Browser_Bits' => '0',
+            'Browser_Maker' => 'DomainTools',
+            'Version' => '2.3',
+            'MajorVer' => '2',
+            'MinorVer' => '3',
+            'Platform' => 'unknown',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '0',
+            'Platform_Maker' => 'unknown',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => true,
+            'Device_Name' => 'unknown',
+            'Device_Maker' => 'unknown',
+            'Device_Type' => 'unknown',
+            'Device_Pointing_Method' => 'unknown',
+            'Device_Code_Name' => 'unknown',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'unknown',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'unknown',
+        ],
+    ],
+];
