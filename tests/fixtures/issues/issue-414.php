8caa9c46252ddc97cda1bc5101c7b886c405f71b
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-09-07 12:29:37 +0200

    add new rule and test

---
+++ tests/fixtures/issues/issue-414.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'issue-414' => [
+        'WeSEE:Ads/PageBot (http://www.wesee.com/bot/)',
+        [
+            'Browser' => 'WeSEE:Ads',
+            'Browser_Type' => 'Bot/Crawler',
+            'Browser_Bits' => '0',
+            'Browser_Maker' => 'WeSEE',
+            'Version' => '0.0',
+            'MajorVer' => '0',
+            'MinorVer' => '0',
+            'Alpha' => true,
+            'Platform' => 'unknown',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '0',
+            'Platform_Maker' => 'unknown',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => true,
+            'Device_Name' => 'unknown',
+            'Device_Maker' => 'unknown',
+            'Device_Type' => 'unknown',
+            'Device_Pointing_Method' => 'unknown',
+            'Device_Code_Name' => 'unknown',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'unknown',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'unknown',
+        ],
+    ],
+];
