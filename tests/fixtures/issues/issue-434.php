abc07a4d58c6b335595d1d82405db3fe69309f72
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-09-20 00:45:00 +0200

    add test

---
+++ tests/fixtures/issues/issue-434.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'issue-434' => [
+        'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:28.0) Gecko/20100101 Firefox/28.0',
+        [
+            'Browser' => 'Firefox',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Mozilla Foundation',
+            'Version' => '28.0',
+            'MajorVer' => '28',
+            'MinorVer' => '0',
+            'Platform' => 'Linux',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Linux Foundation',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => false,
+            'Device_Name' => 'Linux Desktop',
+            'Device_Maker' => 'Various',
+            'Device_Type' => 'Desktop',
+            'Device_Pointing_Method' => 'mouse',
+            'Device_Code_Name' => 'Linux Desktop',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'Gecko',
+            'RenderingEngine_Version' => '28.0',
+            'RenderingEngine_Maker' => 'Mozilla Foundation',
+        ],
+    ],
+];
