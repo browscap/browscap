93592dbe2a89911c3d87b6ce33cdf865e92e8430
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-09-22 20:58:40 +0200

    add new rule and test

---
+++ tests/fixtures/issues/issue-439.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'issue-439' => [
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36 HubSpot Webcrawler',
+        [
+            'Browser' => 'HubSpot Webcrawler',
+            'Browser_Type' => 'Bot/Crawler',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'HubSpot, Inc.',
+            'Version' => '0.0',
+            'MajorVer' => '0',
+            'MinorVer' => '0',
+            'Platform' => 'WinXP',
+            'Platform_Version' => '5.1',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Microsoft Corporation',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => true,
+            'Device_Name' => 'Windows Desktop',
+            'Device_Maker' => 'Various',
+            'Device_Type' => 'Desktop',
+            'Device_Pointing_Method' => 'mouse',
+            'Device_Code_Name' => 'Windows Desktop',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'WebKit',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Apple Inc',
+        ],
+    ],
+];
