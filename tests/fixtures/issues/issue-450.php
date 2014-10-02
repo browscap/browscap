bbd3b52fcfa9007bd3fa5c74a535c2910351eed1
aumel <aurelienmelot@gmail.com>
2014-10-02 18:25:01 +0200

    add new Galaxy Note S3 and test - fix #450

---
+++ tests/fixtures/issues/issue-450.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+	'issue-450' => [
+		'Mozilla/5.0 (Linux; Android 4.4.2; SM-N900 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.117 Mobile Safari/537.36',
+		[
+			'Browser' => 'Chrome',
+			'Browser_Type' => 'Browser',
+			'Browser_Bits' => '32',
+			'Browser_Maker' => 'Google Inc',
+			'Version' => '37.0',
+			'MajorVer' => '37',
+			'MinorVer' => '0',
+			'Platform' => 'Android',
+			'Platform_Version' => '4.4',
+			'Platform_Bits' => '32',
+			'Platform_Maker' => 'Google Inc',
+			'isMobileDevice' => true,
+			'isTablet' => false,
+			'Crawler' => false,
+			'Device_Name' => 'Galaxy Note 3 3G',
+			'Device_Maker' => 'Samsung',
+			'Device_Type' => 'Mobile Phone',
+			'Device_Pointing_Method' => 'touchscreen',
+			'Device_Code_Name' => 'SM-N900',
+			'Device_Brand_Name' => 'Samsung',
+			'RenderingEngine_Name' => 'Blink',
+			'RenderingEngine_Version' => 'unknown',
+			'RenderingEngine_Maker' => 'Google Inc',
+		],
+	],
+];
