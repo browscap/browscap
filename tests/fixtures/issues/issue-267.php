ac5fe575a965beed4e5513d46ea081e4744f4e4e
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-10-06 23:20:04 +0200

    change tests

--- tests/fixtures/issues/issue-267.php
+++ tests/fixtures/issues/issue-267.php
@@ -643,7 +643,7 @@ return [
             'Device_Code_Name' => 'Windows Desktop',
             'Device_Brand_Name' => 'unknown',
             'RenderingEngine_Name' => 'Gecko',
-            'RenderingEngine_Version' => '1.8',
+            'RenderingEngine_Version' => '1.0',
             'RenderingEngine_Maker' => 'Mozilla Foundation',
         ],
     ],
