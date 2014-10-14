1d9224160870312fba87481d8b56150b1536b79b
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-10-14 21:18:50 +0200

    add new rule, extract rules into a separate file, add a test

---
+++ tests/fixtures/issues/issue-460.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'issue-460' => [
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko; Google Web Preview) Chrome/27.0.1453 Safari/537.36',
+        [
+            'Browser' => 'Google Web Preview',
+            'Browser_Type' => 'Bot/Crawler',
+            'Browser_Bits' => '64',
+            'Browser_Maker' => 'Google Inc',
+            'Version' => '0.0',
+            'MajorVer' => '0',
+            'MinorVer' => '0',
+            'Platform' => 'Linux',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '64',
+            'Platform_Maker' => 'Linux Foundation',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => true,
+            'Device_Name' => 'Linux Desktop',
+            'Device_Maker' => 'Various',
+            'Device_Type' => 'Desktop',
+            'Device_Pointing_Method' => 'mouse',
+            'Device_Code_Name' => 'Linux Desktop',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'unknown',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'unknown',
+        ],
+    ],
+];
