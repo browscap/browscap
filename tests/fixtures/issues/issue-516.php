6800e0f47c1b9bf616ac37c388f1b93074b5aaff
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-12-04 20:31:54 +0100

    #516: add new rule and test

---
+++ tests/fixtures/issues/issue-516.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'issue-516' => [
+        'AdsBot-Google-Mobile (+http://www.google.com/mobile/adsbot.html) Mozilla (iPhone; U; CPU iPhone OS 3 0 like Mac OS X) AppleWebKit (KHTML, like Gecko) Mobile Safari',
+        [
+            'Browser' => 'AdsBot Google-Mobile',
+            'Browser_Type' => 'Bot/Crawler',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Google Inc',
+            'Version' => '0.0',
+            'MajorVer' => '0',
+            'MinorVer' => '0',
+            'Platform' => 'iOS',
+            'Platform_Version' => '3.0',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Apple Inc',
+            'isMobileDevice' => true,
+            'isTablet' => false,
+            'Crawler' => false,
+            'Device_Name' => 'iPhone',
+            'Device_Maker' => 'Apple Inc',
+            'Device_Type' => 'Mobile Phone',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'iPhone',
+            'Device_Brand_Name' => 'Apple',
+            'RenderingEngine_Name' => 'WebKit',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Apple Inc',
+        ],
+    ],
+];
