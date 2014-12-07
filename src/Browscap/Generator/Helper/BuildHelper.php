7d526c00bb3adb4ca26b90790a5ac901a333bd5b
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2014-12-07 22:03:26 +0100

    #508: revert some changes

--- src/Browscap/Generator/Helper/BuildHelper.php
+++ src/Browscap/Generator/Helper/BuildHelper.php
@@ -47,185 +47,174 @@ class BuildHelper
     public static function run($version, $resourceFolder, LoggerInterface $logger, WriterCollection $writerCollection,
         CollectionCreator $collectionCreator)
     {
-        gc_collect_cycles();
-        gc_disable();
+        $logger->info('started creating a data collection');
 
-        try {
-            $logger->info('started creating a data collection');
+        $dataCollection = new DataCollection($version);
+        $dataCollection->setLogger($logger);
 
-            $dataCollection = new DataCollection($version);
-            $dataCollection->setLogger($logger);
+        $collectionCreator
+            ->setLogger($logger)
+            ->setDataCollection($dataCollection)
+        ;
 
-            $collectionCreator
-                ->setLogger($logger)
-                ->setDataCollection($dataCollection)
-            ;
+        $collection = $collectionCreator->createDataCollection($resourceFolder);
 
-            $collection = $collectionCreator->createDataCollection($resourceFolder);
+        $logger->info('finished creating a data collection');
 
-            $logger->info('finished creating a data collection');
+        $logger->info('started initialisation of writers');
 
-            $logger->info('started initialisation of writers');
+        $expander = new Expander();
+        $expander
+            ->setDataCollection($collection)
+            ->setLogger($logger)
+        ;
 
-            $expander = new Expander();
-            $expander
-                ->setDataCollection($collection)
-                ->setLogger($logger)
-            ;
+        $logger->info('finished initialisation of writers');
 
-            $logger->info('finished initialisation of writers');
+        $logger->info('started output of header and version');
 
-            $logger->info('started output of header and version');
+        $comments = array(
+            'Provided courtesy of http://browscap.org/',
+            'Created on ' . $collection->getGenerationDate()->format('l, F j, Y \a\t h:i A T'),
+            'Keep up with the latest goings-on with the project:',
+            'Follow us on Twitter <https://twitter.com/browscap>, or...',
+            'Like us on Facebook <https://facebook.com/browscap>, or...',
+            'Collaborate on GitHub <https://github.com/browscap>, or...',
+            'Discuss on Google Groups <https://groups.google.com/forum/#!forum/browscap>.'
+        );
 
-            $comments = array(
-                'Provided courtesy of http://browscap.org/',
-                'Created on ' . $collection->getGenerationDate()->format('l, F j, Y \a\t h:i A T'),
-                'Keep up with the latest goings-on with the project:',
-                'Follow us on Twitter <https://twitter.com/browscap>, or...',
-                'Like us on Facebook <https://facebook.com/browscap>, or...',
-                'Collaborate on GitHub <https://github.com/browscap>, or...',
-                'Discuss on Google Groups <https://groups.google.com/forum/#!forum/browscap>.'
-            );
+        $writerCollection
+            ->fileStart()
+            ->renderHeader($comments)
+            ->renderVersion($version, $collection)
+        ;
 
-            $writerCollection
-                ->fileStart()
-                ->renderHeader($comments)
-                ->renderVersion($version, $collection)
-            ;
+        $logger->info('finished output of header and version');
 
-            $logger->info('finished output of header and version');
+        $output = array();
 
-            $output = array();
+        $logger->info('started output of divisions');
 
-            $logger->info('started output of divisions');
+        $division = $collection->getDefaultProperties();
 
-            $division = $collection->getDefaultProperties();
+        $logger->info('handle division ' . $division->getName());
 
-            $logger->info('handle division ' . $division->getName());
+        $writerCollection
+            ->renderAllDivisionsHeader($collection)
+            ->renderDivisionHeader($division->getName())
+        ;
 
-            $writerCollection
-                ->renderAllDivisionsHeader($collection)
-                ->renderDivisionHeader($division->getName())
-            ;
+        $ua       = $division->getUserAgents();
+        $sections = array($ua[0]['userAgent'] => $ua[0]['properties']);
 
-            $ua       = $division->getUserAgents();
-            $sections = array($ua[0]['userAgent'] => $ua[0]['properties']);
-
-            foreach (array_keys($sections) as $sectionName) {
-                $section = $sections[$sectionName];
-
-                $writerCollection
-                    ->renderSectionHeader($sectionName)
-                    ->renderSectionBody($section, $collection, $sections, $sectionName)
-                    ->renderSectionFooter($sectionName)
-                ;
-            }
-
-            $writerCollection->renderDivisionFooter();
-
-            foreach ($collection->getDivisions() as $division) {
-                /** @var \Browscap\Data\Division $division */
-
-                // run checks on division before expanding versions because the checked properties do not change between
-                // versions
-                $sections = $expander->expand($division, $division->getName());
-
-                foreach (array_keys($sections) as $sectionName) {
-                    $section = $sections[$sectionName];
-
-                    $logger->info('checking division ' . $division->getName());
-
-                    $collection->checkProperty($sectionName, $section);
-                }
-
-                $writerCollection->setSilent($division);
-
-                $versions = $division->getVersions();
-
-                foreach ($versions as $version) {
-                    list($majorVer, $minorVer) = $expander->getVersionParts($version);
-
-                    $divisionName = $expander->parseProperty($division->getName(), $majorVer, $minorVer);
-
-                    $logger->info('handle division ' . $divisionName);
-
-                    $encodedSections = json_encode($sections);
-                    $encodedSections = $expander->parseProperty($encodedSections, $majorVer, $minorVer);
-
-                    $sectionsWithVersion = json_decode($encodedSections, true);
-                    $firstElement        = current($sectionsWithVersion);
-
-                    $writerCollection->renderDivisionHeader($divisionName, $firstElement['Parent']);
-
-                    foreach (array_keys($sectionsWithVersion) as $sectionName) {
-                        if (array_key_exists($sectionName, $output)) {
-                            throw new \UnexpectedValueException(
-                                'tried to add section "' . $sectionName . '" more than once'
-                            );
-                        }
-
-                        $section = $sectionsWithVersion[$sectionName];
-
-                        $writerCollection
-                            ->renderSectionHeader($sectionName)
-                            ->renderSectionBody($section, $collection, $sectionsWithVersion, $sectionName)
-                            ->renderSectionFooter($sectionName)
-                        ;
-
-                        $output[$sectionName] = $sectionName;
-                    }
-
-                    $writerCollection->renderDivisionFooter();
-
-                    unset($divisionName, $majorVer, $minorVer);
-                }
-            }
-
-            $division = $collection->getDefaultBrowser();
-
-            $logger->info('handle division ' . $division->getName());
-
-            $writerCollection->renderDivisionHeader($division->getName());
-
-            $ua       = $division->getUserAgents();
-            $sections = array(
-                $ua[0]['userAgent'] => array_merge(
-                    array('Parent' => 'DefaultProperties'),
-                    $ua[0]['properties']
-                )
-            );
-
-            foreach (array_keys($sections) as $sectionName) {
-                $section = $sections[$sectionName];
-                
-                $writerCollection
-                    ->renderSectionHeader($sectionName)
-                    ->renderSectionBody($section, $collection, $sections, $sectionName)
-                    ->renderSectionFooter($sectionName)
-                ;
-            }
+        foreach (array_keys($sections) as $sectionName) {
+            $section = $sections[$sectionName];
 
             $writerCollection
-                ->renderDivisionFooter()
-                ->renderAllDivisionsFooter()
+                ->renderSectionHeader($sectionName)
+                ->renderSectionBody($section, $collection, $sections, $sectionName)
+                ->renderSectionFooter($sectionName)
             ;
-
-            $logger->info('finished output of divisions');
-
-            $logger->info('started closing writers');
-
-            $writerCollection
-                ->fileEnd()
-                ->close()
-            ;
-
-            $logger->info('finished closing writers');
-        } catch (\Exception $e) {
-            gc_enable();
-
-            throw $e;
         }
 
-        gc_enable();
+        $writerCollection->renderDivisionFooter();
+
+        foreach ($collection->getDivisions() as $division) {
+            /** @var \Browscap\Data\Division $division */
+
+            // run checks on division before expanding versions because the checked properties do not change between
+            // versions
+            $sections = $expander->expand($division, $division->getName());
+
+            foreach (array_keys($sections) as $sectionName) {
+                $section = $sections[$sectionName];
+
+                $logger->info('checking division ' . $division->getName());
+
+                $collection->checkProperty($sectionName, $section);
+            }
+
+            $writerCollection->setSilent($division);
+
+            $versions = $division->getVersions();
+
+            foreach ($versions as $version) {
+                list($majorVer, $minorVer) = $expander->getVersionParts($version);
+
+                $divisionName = $expander->parseProperty($division->getName(), $majorVer, $minorVer);
+
+                $logger->info('handle division ' . $divisionName);
+
+                $encodedSections = json_encode($sections);
+                $encodedSections = $expander->parseProperty($encodedSections, $majorVer, $minorVer);
+
+                $sectionsWithVersion = json_decode($encodedSections, true);
+                $firstElement        = current($sectionsWithVersion);
+
+                $writerCollection->renderDivisionHeader($divisionName, $firstElement['Parent']);
+
+                foreach (array_keys($sectionsWithVersion) as $sectionName) {
+                    if (array_key_exists($sectionName, $output)) {
+                        throw new \UnexpectedValueException(
+                            'tried to add section "' . $sectionName . '" more than once'
+                        );
+                    }
+
+                    $section = $sectionsWithVersion[$sectionName];
+
+                    $writerCollection
+                        ->renderSectionHeader($sectionName)
+                        ->renderSectionBody($section, $collection, $sectionsWithVersion, $sectionName)
+                        ->renderSectionFooter($sectionName)
+                    ;
+
+                    $output[$sectionName] = $sectionName;
+                }
+
+                $writerCollection->renderDivisionFooter();
+
+                unset($divisionName, $majorVer, $minorVer);
+            }
+        }
+
+        $division = $collection->getDefaultBrowser();
+
+        $logger->info('handle division ' . $division->getName());
+
+        $writerCollection->renderDivisionHeader($division->getName());
+
+        $ua       = $division->getUserAgents();
+        $sections = array(
+            $ua[0]['userAgent'] => array_merge(
+                array('Parent' => 'DefaultProperties'),
+                $ua[0]['properties']
+            )
+        );
+
+        foreach (array_keys($sections) as $sectionName) {
+            $section = $sections[$sectionName];
+
+            $writerCollection
+                ->renderSectionHeader($sectionName)
+                ->renderSectionBody($section, $collection, $sections, $sectionName)
+                ->renderSectionFooter($sectionName)
+            ;
+        }
+
+        $writerCollection
+            ->renderDivisionFooter()
+            ->renderAllDivisionsFooter()
+        ;
+
+        $logger->info('finished output of divisions');
+
+        $logger->info('started closing writers');
+
+        $writerCollection
+            ->fileEnd()
+            ->close()
+        ;
+
+        $logger->info('finished closing writers');
     }
 }
