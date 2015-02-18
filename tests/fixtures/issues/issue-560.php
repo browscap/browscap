42c4c6bcc4189c3b4634129358dc9a19f69f93f3
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2015-02-18 21:42:22 +0100

    #560: add test

---
+++ tests/fixtures/issues/issue-560.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'issue-560' => [
+        'Mozilla/5.0 (Mobile; rv:32.0) Gecko/32.0 Firefox/32.0',
+        [
+            'Browser' => 'Firefox',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Mozilla Foundation',
+            'Version' => '32.0',
+            'MajorVer' => '32',
+            'MinorVer' => '0',
+            'Platform' => 'FirefoxOS',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Mozilla Foundation',
+            'isMobileDevice' => true,
+            'isTablet' => false,
+            'Crawler' => false,
+            'JavaScript' => true,
+            'Cookies' => true,
+            'Frames' => true,
+            'IFrames' => true,
+            'Tables' => true,
+            'Device_Name' => 'general Mobile Device',
+            'Device_Maker' => 'Various',
+            'Device_Type' => 'Mobile Phone',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'general Mobile Device',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'Gecko',
+            'RenderingEngine_Version' => '32.0',
+            'RenderingEngine_Maker' => 'Mozilla Foundation',
+        ],
+    ],
+];
