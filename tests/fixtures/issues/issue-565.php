df14bf05454fe08ceb42a071ee5b103d6ff8f21a
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2015-03-02 20:06:48 +0100

    #565: add new browser versions, add test

---
+++ tests/fixtures/issues/issue-565.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'issue-565' => [
+        'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.11 Safari/537.36',
+        [
+            'Browser' => 'Chrome',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Google Inc',
+            'Version' => '42.0',
+            'MajorVer' => '42',
+            'MinorVer' => '0',
+            'Platform' => 'Win8.1',
+            'Platform_Version' => '6.3',
+            'Platform_Bits' => '64',
+            'Platform_Maker' => 'Microsoft Corporation',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => false,
+            'JavaScript' => true,
+            'Cookies' => true,
+            'Frames' => true,
+            'IFrames' => true,
+            'Tables' => true,
+            'Device_Name' => 'Windows Desktop',
+            'Device_Maker' => 'Various',
+            'Device_Type' => 'Desktop',
+            'Device_Pointing_Method' => 'mouse',
+            'Device_Code_Name' => 'Windows Desktop',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'Blink',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Google Inc',
+        ],
+    ],
+];
