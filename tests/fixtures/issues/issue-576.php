334ba92a608739728d5ee2fa21a80e23268bccb1
mimmi20 <t_mueller_stolzenhain@yahoo.de>
2015-03-11 21:41:20 +0100

    #576: add new rules and test

---
+++ tests/fixtures/issues/issue-576.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'issue-576' => [
+        'Mozilla/5.0 (compatible; SetLinks bot 1.2)',
+        [
+            'Browser' => 'SetLinks.ru Crawler',
+            'Browser_Type' => 'Bot/Crawler',
+            'Browser_Bits' => '0',
+            'Browser_Maker' => 'SetLinks.ru',
+            'Version' => '1.2',
+            'MajorVer' => '1',
+            'MinorVer' => '2',
+            'Platform' => 'unknown',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '0',
+            'Platform_Maker' => 'unknown',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => true,
+            'JavaScript' => false,
+            'Cookies' => false,
+            'Frames' => false,
+            'IFrames' => false,
+            'Tables' => false,
+            'Device_Name' => 'unknown',
+            'Device_Maker' => 'unknown',
+            'Device_Type' => 'unknown',
+            'Device_Pointing_Method' => 'unknown',
+            'Device_Code_Name' => 'unknown',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'unknown',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'unknown',
+        ],
+    ],
+];
