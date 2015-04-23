11bd1ac4232f82cdfc3741b7c528dca3b0a366eb
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2015-04-23 22:26:01 +0200

    #630: add new rule and test

---
+++ tests/fixtures/issues/issue-630.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'issue-630' => [
+        'mbot v.1.16',
+        [
+            'Browser' => 'mbot',
+            'Browser_Type' => 'Bot/Crawler',
+            'Browser_Bits' => '0',
+            'Browser_Maker' => 'unknown',
+            'Version' => '1.16',
+            'MajorVer' => '1',
+            'MinorVer' => '16',
+            'Platform' => 'unknown',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '0',
+            'Platform_Maker' => 'unknown',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => true,
+            'JavaScript' => false,
+            'Cookies' => false,
+            'Frames' => false,
+            'IFrames' => false,
+            'Tables' => false,
+            'Device_Name' => 'unknown',
+            'Device_Maker' => 'unknown',
+            'Device_Type' => 'unknown',
+            'Device_Pointing_Method' => 'unknown',
+            'Device_Code_Name' => 'unknown',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'unknown',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'unknown',
+        ],
+    ],
+];
