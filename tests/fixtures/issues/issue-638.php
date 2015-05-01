23fb73e702434fec04e0df5ea9ead5bb76c55506
Thomas Müller <mimmi20@live.de>
2015-05-01 20:18:40 +0200

    #636: add new application, add new iTunes version, add vivaldi on windows, add tests

---
+++ tests/fixtures/issues/issue-638.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'issue-638' => [
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.89 Vivaldi/1.0.83.38 Safari/537.36',
+        [
+            'Browser' => 'Vivaldi',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Vivaldi Technologies',
+            'Version' => '1.0',
+            'MajorVer' => '1',
+            'MinorVer' => '0',
+            'Platform' => 'Win7',
+            'Platform_Version' => '6.1',
+            'Platform_Bits' => '64',
+            'Platform_Maker' => 'Microsoft Corporation',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => false,
+            'JavaScript' => true,
+            'Cookies' => true,
+            'Frames' => true,
+            'IFrames' => true,
+            'Tables' => true,
+            'Device_Name' => 'Windows Desktop',
+            'Device_Maker' => 'Various',
+            'Device_Type' => 'Desktop',
+            'Device_Pointing_Method' => 'mouse',
+            'Device_Code_Name' => 'Windows Desktop',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'Blink',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Google Inc',
+        ],
+    ],
+];
