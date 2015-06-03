968947959ac709ea9c98e2c49f70d0e0a0ca2bac
Thomas Müller <mimmi20@live.de>
2015-06-03 22:55:10 +0200

    #666: fix test

--- tests/fixtures/issues/issue-666.php
+++ tests/fixtures/issues/issue-666.php
@@ -37,6 +37,6 @@ return [
             'RenderingEngine_Version' => '7.0',
             'RenderingEngine_Maker' => 'Microsoft Corporation',
         ],
-        'lite' => true,
+        'lite' => false,
     ],
 ];
