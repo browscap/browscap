bb75ecba6a566aa1f479021eed4977ce6bec313c
Thomas Müller <mimmi20@live.de>
2015-09-19 20:43:52 +0200

    #737: add new test, change rules

---
+++ tests/fixtures/issues/issue-737.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'issue-737' => [
+        'Mozilla/5.0 (Linux; Android 5.0.2; SM-T800 Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.84 Safari/537.36',
+        [
+            'Browser' => 'Chrome',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Google Inc',
+            'Version' => '45.0',
+            'MajorVer' => '45',
+            'MinorVer' => '0',
+            'Platform' => 'Android',
+            'Platform_Version' => '5.0',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Google Inc',
+            'isMobileDevice' => true,
+            'isTablet' => true,
+            'Crawler' => false,
+            'JavaScript' => true,
+            'Cookies' => true,
+            'Frames' => true,
+            'IFrames' => true,
+            'Tables' => true,
+            'VBScript' => false,
+            'JavaApplets' => false,
+            'ActiveXControls' => false,
+            'BackgroundSounds' => false,
+            'Device_Name' => 'Galaxy Tab S 10.5',
+            'Device_Maker' => 'Samsung',
+            'Device_Type' => 'Tablet',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'SM-T800',
+            'Device_Brand_Name' => 'Samsung',
+            'RenderingEngine_Name' => 'Blink',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Google Inc',
+        ],
+        'lite' => true,
+    ]
+];
