8897cac30795f70a322f52435aa2531cd87454ac
Thomas Müller <mimmi20@live.de>
2015-09-21 20:05:22 +0200

    #744: add new test

---
+++ tests/fixtures/issues/issue-744.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'issue-744' => [
+        'Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6',
+        [
+            'Browser' => 'Chrome',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Google Inc',
+            'Version' => '45.0',
+            'MajorVer' => '45',
+            'MinorVer' => '0',
+            'Platform' => 'Android',
+            'Platform_Version' => '5.0',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Google Inc',
+            'isMobileDevice' => true,
+            'isTablet' => true,
+            'Crawler' => false,
+            'JavaScript' => true,
+            'Cookies' => true,
+            'Frames' => true,
+            'IFrames' => true,
+            'Tables' => true,
+            'VBScript' => false,
+            'JavaApplets' => false,
+            'ActiveXControls' => false,
+            'BackgroundSounds' => false,
+            'Device_Name' => 'Galaxy Tab S 10.5',
+            'Device_Maker' => 'Samsung',
+            'Device_Type' => 'Tablet',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'SM-T800',
+            'Device_Brand_Name' => 'Samsung',
+            'RenderingEngine_Name' => 'Blink',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Google Inc',
+        ],
+        'lite' => false,
+    ]
+];
