e422838b15c4071a07e1c486cc9383dad9e609e9
Thomas Müller <mimmi20@live.de>
2015-09-27 10:18:14 +0200

    #749: remove duplicate test

--- tests/fixtures/issues/issue-749.php
+++ tests/fixtures/issues/issue-749.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'issue-749-A' => [
+    'issue-749' => [
         '[\'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36 GD\']',
         [
             'Browser' => 'Fake Browser',
@@ -39,42 +39,4 @@ return [
         ],
         'lite' => false,
     ],
-    'issue-749-B' => [
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_1) AppleWebkit/535.1 (KHTML, like Gecko) Chrome/13.0.782.215 Safari/535.1',
-        [
-            'Browser' => 'Chrome',
-            'Browser_Type' => 'Browser',
-            'Browser_Bits' => '32',
-            'Browser_Maker' => 'Mozilla Foundation',
-            'Version' => '2.0',
-            'MajorVer' => '2',
-            'MinorVer' => '0',
-            'Platform' => 'Win32',
-            'Platform_Version' => 'unknown',
-            'Platform_Bits' => '32',
-            'Platform_Maker' => 'Microsoft Corporation',
-            'isMobileDevice' => false,
-            'isTablet' => false,
-            'Crawler' => false,
-            'JavaScript' => true,
-            'Cookies' => true,
-            'Frames' => true,
-            'IFrames' => true,
-            'Tables' => true,
-            'VBScript' => false,
-            'JavaApplets' => true,
-            'ActiveXControls' => false,
-            'BackgroundSounds' => false,
-            'Device_Name' => 'Windows Desktop',
-            'Device_Maker' => 'Various',
-            'Device_Type' => 'Desktop',
-            'Device_Pointing_Method' => 'mouse',
-            'Device_Code_Name' => 'Windows Desktop',
-            'Device_Brand_Name' => 'unknown',
-            'RenderingEngine_Name' => 'Gecko',
-            'RenderingEngine_Version' => '1.8',
-            'RenderingEngine_Maker' => 'Mozilla Foundation',
-        ],
-        'lite' => false,
-    ]
 ];
