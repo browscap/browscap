71c0225807559da4108b33cdfb088228fb04f1af
Thomas Müller <mimmi20@live.de>
2015-09-28 21:09:01 +0200

    #758: add new test

---
+++ tests/fixtures/issues/issue-758.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'issue-758' => [
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Mobile/12H321',
+        [
+            'Browser' => 'Firefox',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Mozilla Foundation',
+            'Version' => '2.0',
+            'MajorVer' => '2',
+            'MinorVer' => '0',
+            'Platform' => 'Win32',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Microsoft Corporation',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => false,
+            'JavaScript' => true,
+            'Cookies' => true,
+            'Frames' => true,
+            'IFrames' => true,
+            'Tables' => true,
+            'VBScript' => false,
+            'JavaApplets' => true,
+            'ActiveXControls' => false,
+            'BackgroundSounds' => false,
+            'Device_Name' => 'Windows Desktop',
+            'Device_Maker' => 'Various',
+            'Device_Type' => 'Desktop',
+            'Device_Pointing_Method' => 'mouse',
+            'Device_Code_Name' => 'Windows Desktop',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'Gecko',
+            'RenderingEngine_Version' => '1.8',
+            'RenderingEngine_Maker' => 'Mozilla Foundation',
+        ],
+        'lite' => false,
+    ]
+];
