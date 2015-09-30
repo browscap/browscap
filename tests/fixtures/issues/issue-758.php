2ff01aa12f3831754eeefe41b16a52ebd09cdcd5
Sven Kummer <skummer@rapidsoft.de>
2015-09-30 13:33:28 +0200

    Added test for fix 758

---
+++ tests/fixtures/issues/issue-758.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+    'issue-758A' => [
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Mobile/12H321',
+        [
+            'Browser' => 'Safari',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Apple Inc',
+            'Version' => '0.0',
+            'MajorVer' => '0',
+            'MinorVer' => '0',
+            'Platform' => 'iOS',
+            'Platform_Version' => '8.4',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Apple Inc',
+            'isMobileDevice' => true,
+            'isTablet' => false,
+            'Crawler' => false,
+            'Device_Name' => 'iPhone',
+            'Device_Maker' => 'Apple Inc',
+            'Device_Type' => 'Mobile Phone',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'iPhone',
+            'Device_Brand_Name' => 'Apple',
+            'RenderingEngine_Name' => 'WebKit',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Apple Inc'
+        ],
+        'lite' => false,
+    ],
+    'issue-758B' => [
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4',
+        [
+            'Browser' => 'Safari',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Apple Inc',
+            'Version' => '8.0',
+            'MajorVer' => '8',
+            'MinorVer' => '0',
+            'Platform' => 'iOS',
+            'Platform_Version' => '8.4',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Apple Inc',
+            'isMobileDevice' => true,
+            'isTablet' => false,
+            'Crawler' => false,
+            'Device_Name' => 'iPhone',
+            'Device_Maker' => 'Apple Inc',
+            'Device_Type' => 'Mobile Phone',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'iPhone',
+            'Device_Brand_Name' => 'Apple',
+            'RenderingEngine_Name' => 'WebKit',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Apple Inc'
+        ],
+        'lite' => false,
+    ]
+];
