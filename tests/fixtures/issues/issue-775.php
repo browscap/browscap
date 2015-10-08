ef8e1b1b9c93d88a2765879da85ac21291ba4ace
Thomas Müller <mimmi20@live.de>
2015-10-08 22:50:39 +0200

    #775: add new test

---
+++ tests/fixtures/issues/issue-775.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'issue-775' => [
+        'Mozilla/5.0 (Mobile: Windows Phone 8.1; Android 4.0; ARM; Trident/7.0; Touch; rv:11.0; IEMobile/11.0; Nokia; Lumia 730 Dual Sim',
+        [
+            'Browser' => 'IEMobile',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Microsoft Corporation',
+            'Version' => '11.0',
+            'MajorVer' => '11',
+            'MinorVer' => '0',
+            'Platform' => 'WinPhone8.1',
+            'Platform_Version' => '8.1',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Microsoft Corporation',
+            'isMobileDevice' => true,
+            'isTablet' => false,
+            'Crawler' => false,
+            'JavaScript' => true,
+            'Cookies' => true,
+            'Frames' => true,
+            'IFrames' => true,
+            'Tables' => true,
+            'VBScript' => false,
+            'JavaApplets' => true,
+            'ActiveXControls' => false,
+            'BackgroundSounds' => false,
+            'Device_Name' => 'Lumia 730',
+            'Device_Maker' => 'Nokia',
+            'Device_Type' => 'Mobile Phone',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'Lumia 730',
+            'Device_Brand_Name' => 'Nokia',
+            'RenderingEngine_Name' => 'Trident',
+            'RenderingEngine_Version' => '7.0',
+            'RenderingEngine_Maker' => 'Microsoft Corporation',
+        ],
+        'lite' => false,
+    ]
+];
