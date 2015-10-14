e41b918e5ae8a2400358afa030c190678fac7a5f
Thomas Müller <mimmi20@live.de>
2015-10-14 11:36:50 +0200

    #790: add new test

---
+++ tests/fixtures/issues/issue-790.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'issue-790' => [
+        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36 OPR/31.0.1889.174',
+        [
+            'Browser' => 'Opera',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Opera Software ASA',
+            'Version' => '31.0',
+            'MajorVer' => '31',
+            'MinorVer' => '0',
+            'Platform' => 'Win10',
+            'Platform_Version' => '10.0',
+            'Platform_Bits' => '64',
+            'Platform_Maker' => 'Microsoft Corporation',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => false,
+            'JavaScript' => true,
+            'Cookies' => true,
+            'Frames' => true,
+            'IFrames' => true,
+            'Tables' => true,
+            'VBScript' => false,
+            'JavaApplets' => false,
+            'ActiveXControls' => false,
+            'BackgroundSounds' => false,
+            'Device_Name' => 'Windows Desktop',
+            'Device_Maker' => 'Various',
+            'Device_Type' => 'Desktop',
+            'Device_Pointing_Method' => 'mouse',
+            'Device_Code_Name' => 'Windows Desktop',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'Blink',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Google Inc',
+        ],
+        'lite' => true,
+    ]
+];
