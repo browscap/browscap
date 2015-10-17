075a728a294bcf59e5a07cf24144310adcb9c8c5
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2015-10-17 18:13:20 +0200

    #789: add new test

---
+++ tests/fixtures/issues/issue-789.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'issue-789' => [
+        'Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; NOKIA; Lumia 930) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Mobile Safari/537.36 Edge/12.0',
+        [
+            'Browser' => 'Edge Mobile',
+            'Browser_Type' => 'Browser',
+            'Browser_Bits' => '32',
+            'Browser_Maker' => 'Microsoft Corporation',
+            'Version' => '12.0',
+            'MajorVer' => '12',
+            'MinorVer' => '0',
+            'Platform' => 'WinPhone10',
+            'Platform_Version' => '10.0',
+            'Platform_Bits' => '32',
+            'Platform_Maker' => 'Microsoft Corporation',
+            'isMobileDevice' => true,
+            'isTablet' => false,
+            'Crawler' => false,
+            'JavaScript' => true,
+            'Cookies' => true,
+            'Frames' => true,
+            'IFrames' => true,
+            'Tables' => true,
+            'VBScript' => false,
+            'JavaApplets' => false,
+            'ActiveXControls' => false,
+            'BackgroundSounds' => false,
+            'Device_Name' => 'Lumia 930',
+            'Device_Maker' => 'Nokia',
+            'Device_Type' => 'Mobile Phone',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'Lumia 930',
+            'Device_Brand_Name' => 'Nokia',
+            'RenderingEngine_Name' => 'Edge',
+            'RenderingEngine_Version' => '12.0',
+            'RenderingEngine_Maker' => 'Microsoft Corporation',
+        ],
+        'lite' => false,
+    ]
+];
