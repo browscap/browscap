18566bddabd5829c9844a9f7714b0928df910cff
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2015-10-25 11:14:38 +0100

    #797: fix invalid json, fix tests

--- tests/fixtures/issues/issue-533.php
+++ tests/fixtures/issues/issue-533.php
@@ -24,7 +24,7 @@ return [
             'IFrames' => false,
             'Tables' => false,
             'VBScript' => false,
-            'JavaApplets' => false,
+            'JavaApplets' => true,
             'ActiveXControls' => false,
             'BackgroundSounds' => false,
             'Device_Name' => 'Windows Desktop',
