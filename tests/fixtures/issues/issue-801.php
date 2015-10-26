e19146d32ef37cae29ed78f1c51420643be21de9
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2015-10-26 21:38:49 +0100

    #801: fix test key

--- tests/fixtures/issues/issue-801.php
+++ tests/fixtures/issues/issue-801.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'issue-797' => [
+    'issue-801' => [
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/601.2.7 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.7',
         [
             'Browser' => 'Safari',
