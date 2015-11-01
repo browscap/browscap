b351b8b5d1c753638d97eb231ef76f71aeab2334
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2015-11-01 08:56:20 +0100

    #816: change rules and tests

--- tests/fixtures/issues/issue-618.php
+++ tests/fixtures/issues/issue-618.php
@@ -2264,7 +2264,7 @@ return [
             'IFrames' => true,
             'Tables' => true,
             'VBScript' => false,
-            'JavaApplets' => false,
+            'JavaApplets' => true,
             'ActiveXControls' => false,
             'BackgroundSounds' => false,
             'Device_Name' => 'Windows Desktop',
