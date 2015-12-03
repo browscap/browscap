d109fa5e0433caa683515edb68a363a73bff314d
Paris Holley <mail@parisholley.com>
2015-12-02 20:31:40 -0800

    first try

---
+++ tests/fixtures/issues/issue-827.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'issue-827-A' => [
+        'Mozilla/5.0 (X11; Linux x86_64) adbeat.com/policy AppleWebKit/537.21 (KHTML, like Gecko) Firefox/34.0 Safari/537.21',
+        [
+            'Browser' => 'Adbeat Bot',
+            'Browser_Type' => 'Bot/Crawler',
+            'Browser_Bits' => '64',
+            'Browser_Maker' => 'adbeat.com',
+            'Version' => '0.0',
+            'MajorVer' => '0',
+            'MinorVer' => '0',
+            'Platform' => 'Linux',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '64',
+            'Platform_Maker' => 'Linux Foundation',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => true,
+            'JavaScript' => false,
+            'Cookies' => false,
+            'Frames' => false,
+            'IFrames' => false,
+            'Tables' => false,
+            'Device_Name' => 'Linux Desktop',
+            'Device_Maker' => 'Various',
+            'Device_Type' => 'Desktop',
+            'Device_Pointing_Method' => 'mouse',
+            'Device_Code_Name' => 'Linux Desktop',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'unknown',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'unknown',
+        ],
+        'lite' => false,
+        'standard' => true
+    ]
+];
