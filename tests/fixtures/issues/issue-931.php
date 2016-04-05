efd6d1ec49f43a68d34cbc6eb4c0ca10c897e0c3
Chris Hawes <chris.hawes@basekit.com>
2016-04-05 12:44:42 +0100

    Add Nexus 6 Android 6.0.1 user agent string. Fixes #931

---
+++ tests/fixtures/issues/issue-931.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'issue-931' => [
+        'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 6 Build/MMB29V) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.105 Mobile Safari/537.36',
+        [
+            'Browser' => 'Chrome',
+            'Browser_Type' => 'Browser',
+            'Browser_Maker' => 'Google Inc',
+            'Version' => '49.0',
+            'MajorVer' => '49',
+            'MinorVer' => '0',
+            'Platform' => 'Android',
+            'Platform_Version' => '6.0.1',
+            'Platform_Maker' => 'Google Inc',
+            'Device_Name' => 'Nexus 6',
+            'Device_Maker' => 'Motorola',
+            'Device_Type' => 'Mobile Phone',
+            'Device_Pointing_Method' => 'touchscreen',
+            'Device_Code_Name' => 'Nexus 6',
+            'Device_Brand_Name' => 'Google',
+            'RenderingEngine_Name' => 'Blink',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'Google Inc',
+        ],
+        'lite'     => false,
+        'standard' => true,
+    ],
+];
