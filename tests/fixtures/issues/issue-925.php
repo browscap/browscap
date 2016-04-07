9252d313cc48ed344f15d22388f8f6caadeb485c
Thomas Müller <mimmi20@live.de>
2016-04-07 22:50:21 +0200

    #925: add new test

---
+++ tests/fixtures/issues/issue-925.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'issue-925' => [
+        'Comodo SSL Checker',
+        [
+            'Browser' => 'Comodo SSL Checker',
+            'Browser_Type' => 'Bot/Crawler',
+            'Browser_Bits' => '0',
+            'Browser_Maker' => 'Comodo Group Inc',
+            'Version' => '0.0',
+            'MajorVer' => '0',
+            'MinorVer' => '0',
+            'Platform' => 'unknown',
+            'Platform_Version' => 'unknown',
+            'Platform_Bits' => '0',
+            'Platform_Maker' => 'unknown',
+            'isMobileDevice' => false,
+            'isTablet' => false,
+            'Crawler' => true,
+            'Device_Name' => 'unknown',
+            'Device_Maker' => 'unknown',
+            'Device_Type' => 'unknown',
+            'Device_Pointing_Method' => 'unknown',
+            'Device_Code_Name' => 'unknown',
+            'Device_Brand_Name' => 'unknown',
+            'RenderingEngine_Name' => 'unknown',
+            'RenderingEngine_Version' => 'unknown',
+            'RenderingEngine_Maker' => 'unknown',
+        ],
+        'lite' => false,
+        'standard' => true,
+    ],
+];
