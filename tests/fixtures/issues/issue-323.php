14de26671f670ea19d1fef1dd2669aa974359183
Thomas Müller <mimmi20@live.de>
2016-04-07 20:10:41 +0200

    #919: add new rules to Opera Generic, disable tests for lite mode

--- tests/fixtures/issues/issue-323.php
+++ tests/fixtures/issues/issue-323.php
@@ -118,7 +118,7 @@ return [
             'RenderingEngine_Version' => 'unknown',
             'RenderingEngine_Maker' => 'Google Inc',
         ],
-        'lite' => true,
+        'lite' => false,
         'standard' => true,
     ],
     'issue-323-E' => [
