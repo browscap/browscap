ad5ce7ca1f632f2f151a7407742835f6c6ae44f6
Thomas Müller <mimmi20@live.de>
2016-05-06 13:47:59 +0200

    #971: fix test

--- tests/fixtures/issues/issue-971.php
+++ tests/fixtures/issues/issue-971.php
@@ -21,7 +21,7 @@ return [
             'Alpha' => false,
             'Beta' => false,
             'Win16' => false,
-            'Win32' => false,
+            'Win32' => true,
             'Win64' => false,
             'Frames' => false,
             'IFrames' => false,
