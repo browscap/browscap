c15a4e4cd7b399534b94cd64ea38a67a61288807
Thomas Müller <mimmi20@live.de>
2016-05-12 22:01:16 +0200

    #981: update some rules and tests

--- tests/fixtures/issues/issue-900.php
+++ tests/fixtures/issues/issue-900.php
@@ -53127,7 +53127,7 @@ return [
             'Alpha' => false,
             'Beta' => false,
             'Win16' => false,
-            'Win32' => true,
+            'Win32' => false,
             'Win64' => false,
             'Frames' => true,
             'IFrames' => true,
