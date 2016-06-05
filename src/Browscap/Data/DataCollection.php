788668af750ea5bf44469fef9be87753c3c3c2ae
Thomas Müller <mimmi20@live.de>
2016-06-05 21:45:14 +0200

    remove platform properties from useragents-section

--- src/Browscap/Data/DataCollection.php
+++ src/Browscap/Data/DataCollection.php
@@ -509,6 +509,10 @@ class DataCollection
             || array_key_exists('Platform_Maker', $properties)
             || array_key_exists('Platform_Bits', $properties)
             || array_key_exists('Platform_Version', $properties)
+            || array_key_exists('Win16', $properties)
+            || array_key_exists('Win32', $properties)
+            || array_key_exists('Win64', $properties)
+            || array_key_exists('Browser_Bits', $properties)
         ) {
             throw new \LogicException($message);
         }
