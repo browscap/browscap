37fb1886932557edda28c8516e01fcc4038d3ac9
Thomas Müller <mimmi20@live.de>
2017-04-02 21:04:37 +0200

    #1318: fix test name

--- tests/fixtures/issues/issue-1318.php
+++ tests/fixtures/issues/issue-1318.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'issue-1313-A' => [
+    'issue-1318-A' => [
         'ua' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12',
         'properties' => [
             'Comment' => 'Edge 12.0',
