e0175a327ccad98a0b5ea789c82df5423d1c7a87
Thomas Müller <thomas.mueller@geld.de>
2017-10-04 10:29:46 +0200

    removed file docblock

--- tests/BrowscapTest/Helper/CollectionCreatorTest.php
+++ tests/BrowscapTest/Helper/CollectionCreatorTest.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * This file is part of the browscap package.
- *
- * Copyright (c) 1998-2017, Browser Capabilities Project
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 declare(strict_types = 1);
 namespace BrowscapTest\Helper;
 
