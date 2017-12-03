d80e717794cd9e23d6909f7415abd43bc650a887
Mikhail Galanin <mikhail.galanin@corp.badoo.com>
2017-12-03 19:26:34 +0000

    Fixed test numbers, review fixes

--- tests/issues/issue-1735.php
+++ tests/issues/issue-1735.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'issue-XXXX-A' => [
+    'issue-1735-A' => [
         'ua' => 'Mozilla/5.0 (Linux; Android 6.0.1; SM-J510FN Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.98 Mobile Safari/537.36',
         'properties' => [
             'Comment' => 'Chrome 61.0',
@@ -44,7 +44,7 @@ return [
         'standard' => true,
         'full' => true,
     ],
-    'issue-XXXX-B' => [
+    'issue-1735-B' => [
         'ua' => 'Mozilla/5.0 (Linux; Android 6.0.1; SAMSUNG SM-J510FN Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/5.4 Chrome/51.0.2704.106 Mobile Safari/537.36',
         'properties' => [
             'Comment' => 'Samsung Browser 5.4',
@@ -87,7 +87,7 @@ return [
         'standard' => false,
         'full' => true,
     ],
-    'issue-XXXX-C' => [
+    'issue-1735-C' => [
         'ua' => 'Mozilla/5.0 (Linux; Android 6.0.1; SM-J510FN Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 YaBrowser/17.7.0.1178.00 Mobile Safari/537.36',
         'properties' => [
             'Comment' => 'Yandex Browser 17.7',
@@ -130,7 +130,7 @@ return [
         'standard' => false,
         'full' => true,
     ],
-    'issue-XXXX-D' => [
+    'issue-1735-D' => [
         'ua' => 'Mozilla/5.0 (Linux; Android 6.0.1; SM-J510FN Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Mobile Safari/537.36',
         'properties' => [
             'Comment' => 'Android WebView 4.0',
@@ -173,7 +173,7 @@ return [
         'standard' => true,
         'full' => true,
     ],
-    'issue-XXXX-E' => [
+    'issue-1735-E' => [
         'ua' => 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; SM-J510FN Build/MMB29M)',
         'properties' => [
             'Comment' => 'Dalvik 2.1',
@@ -216,7 +216,7 @@ return [
         'standard' => true,
         'full' => true,
     ],
-    'issue-XXXX-F' => [
+    'issue-1735-F' => [
         'ua' => 'Mozilla/5.0 (Linux; Android 6.0.1; SM-J510FN Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Mobile Safari/537.36 SznProhlizec/5.1.1a',
         'properties' => [
             'Comment' => 'Seznam Browser 5.1',
@@ -259,7 +259,7 @@ return [
         'standard' => true,
         'full' => true,
     ],
-    'issue-XXXX-G' => [
+    'issue-1735-G' => [
         'ua' => 'Mozilla/5.0 (Linux; U; Android 6.0.1; en-US; SM-J510FN Build/MMB29M) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/11.4.5.1005 U3/0.8.0 Mobile Safari/534.30',
         'properties' => [
             'Comment' => 'UC Browser 11.4',
@@ -296,7 +296,7 @@ return [
             'Device_Brand_Name' => 'Samsung',
             'RenderingEngine_Name' => 'U3',
             'RenderingEngine_Version' => 'unknown',
-            'RenderingEngine_Maker' => 'UCWeb Inc',
+            'RenderingEngine_Maker' => 'UCWeb Inc.',
         ],
         'lite' => false,
         'standard' => true,
