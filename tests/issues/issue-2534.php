73563eac740b497e1684770daec571e2a8110ac2
Paul Rutter <paul@blueconic.com>
2021-11-15 10:08:55 +0100

    #2534 Update Apple platform versions - Add Mac OS X 11 (Big sur) and 12 (Monterey)

--- tests/issues/issue-2534.php
+++ tests/issues/issue-2534.php
@@ -56,4 +56,14 @@ return [
         'standard' => true,
         'full' => true,
     ],
+    'issue-2534-F' => [
+        'ua' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11.0) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/11.0 Safari/602.1.50',
+        'properties' => [
+            "Platform" => "MacOSX",
+            "Platform_Version" => "11.0",
+        ],
+        'lite' => false,
+        'standard' => true,
+        'full' => true,
+    ],
 ];
