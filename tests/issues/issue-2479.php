d47ca04f904b7f47245926cfb783241884a32f5b
Paul Rutter <paul@blueconic.com>
2021-11-15 11:45:08 +0100

    #2534 Update Apple platform versions - Fix unit tests - Edge using different version numbers from 75

--- tests/issues/issue-2479.php
+++ tests/issues/issue-2479.php
@@ -40,7 +40,7 @@ return [
             'RenderingEngine_Version' => 'unknown',
             'RenderingEngine_Maker' => 'Apple Inc',
         ],
-        'lite' => true,
+        'lite' => false,
         'standard' => true,
         'full' => true,
     ],
