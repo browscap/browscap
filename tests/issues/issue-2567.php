4c5c729acbee8101ea4cc3d346c0784e2610ff70
Guillermo López Leal <guillermo.lopezleal@vonage.com>
2022-02-01 13:29:15 +0100

    Add Mobile Safari up to 15.4

---
+++ tests/issues/issue-2567.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types = 1);
+
+return [
+    'issue-2567-A' => [
+        'ua' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1',
+        'properties' => [
+            "Platform"=> "iOS",
+            'Platform_Version' => '15.2',
+            'Platform_Maker' => 'Apple Inc',
+            "Browser_Version" => "Safari",
+            "Browser_Version" => "15.2",
+            "Comment" => "Mobile Safari 15.2",
+            "Device_Name" => "iPhone",
+        ],
+        'lite' => false,
+        'standard' => true,
+        'full' => true,
+    ],
+    'issue-2567-B' => [
+        'ua' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Mobile/15E148 Safari/604.1',
+        'properties' => [
+            "Platform"=> "iOS",
+            'Platform_Version' => '15.3',
+            'Platform_Maker' => 'Apple Inc',
+            "Browser_Version" => "Safari",
+            "Browser_Version" => "15.3",
+            "Comment" => "Mobile Safari 15.3",
+            "Device_Name" => "iPhone",
+        ],
+        'lite' => false,
+        'standard' => true,
+        'full' => true,
+    ],
+    'issue-2567-C' => [
+        'ua' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1',
+        'properties' => [
+            "Platform"=> "iOS",
+            'Platform_Version' => '15.2',
+            'Platform_Maker' => 'Apple Inc',
+            "Browser_Version" => "Safari",
+            "Browser_Version" => "15.2",
+            "Comment" => "Mobile Safari 15.2",
+            "Device_Name" => "iPhone",
+        ],
+        'lite' => false,
+        'standard' => true,
+        'full' => true,
+    ],
+    'issue-2567-D' => [
+        'ua' => 'Mozilla/5.0 (iPad; CPU OS 15_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1',
+        'properties' => [
+            "Platform"=> "ipadOS",
+            'Platform_Version' => '15.2',
+            'Platform_Maker' => 'Apple Inc',
+            "Browser_Version" => "Safari",
+            "Browser_Version" => "15.2",
+            "Comment" => "Mobile Safari 15.2",
+            "Device_Name" => "iPad",
+        ],
+        'lite' => false,
+        'standard' => true,
+        'full' => true,
+    ],
+    'issue-2567-E' => [
+        'ua' => 'Mozilla/5.0 (iPad; CPU OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Mobile/15E148 Safari/604.1',
+        'properties' => [
+            "Platform"=> "ipadOS",
+            'Platform_Version' => '15.3',
+            'Platform_Maker' => 'Apple Inc',
+            "Browser_Version" => "Safari",
+            "Browser_Version" => "15.3",
+            "Comment" => "Mobile Safari 15.3",
+            "Device_Name" => "iPhone",
+        ],
+        'lite' => false,
+        'standard' => true,
+        'full' => true,
+    ],
+];
