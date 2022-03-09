b191e855bb205d5f314a29ebfbfcad507bf69e0d
Paul Rutter <paul@blueconic.com>
2022-03-09 15:48:39 +0100

    Change test name

--- tests/issues/issue-2599.php
+++ tests/issues/issue-2599.php
@@ -30,7 +30,7 @@ return [
         'standard' => true,
         'full' => true,
     ],
-    'issue-2599-A' => [
+    'issue-2599-C' => [
         'ua' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.4758.102 Safari/537.36',
         'properties' => [
             "Platform"=> "Win10",
