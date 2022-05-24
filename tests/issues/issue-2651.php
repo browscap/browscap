2a92701c3430fe40253e72ab46062d44b8c958ad
Guillermo López Leal <guillermo.lopezleal@vonage.com>
2022-05-24 13:10:57 +0200

    Fix test with correct PR name

--- tests/issues/issue-2651.php
+++ tests/issues/issue-2651.php
@@ -2,7 +2,7 @@
 declare(strict_types = 1);
 
 return [
-    'issue-XXXX-A' => [
+    'issue-2651-A' => [
         'ua' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/17.0 Chrome/96.0.4664.104 Safari/537.36',
         'properties' => [
             "Platform"=> "Android",
@@ -14,7 +14,7 @@ return [
         'standard' => true,
         'full' => true,
     ],
-    'issue-XXXX-B' => [
+    'issue-2651-B' => [
         'ua' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/16.2 Chrome/92.0.4515.166 Safari/537.36',
         'properties' => [
             "Platform"=> "Android",
@@ -26,7 +26,7 @@ return [
         'standard' => true,
         'full' => true,
     ],
-    'issue-XXXX-C' => [
+    'issue-2651-C' => [
         'ua' => 'Mozilla/5.0 (Linux; Android 10; SAMSUNG SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/17.0 Chrome/96.0.4664.104 Mobile Safari/537.36',
         'properties' => [
             'Comment' => 'Samsung Browser 17.0',
@@ -37,7 +37,7 @@ return [
         'standard' => true,
         'full' => true,
     ],
-    'issue-XXXX-D' => [
+    'issue-2651-D' => [
         'ua' => 'Mozilla/5.0 (Linux; Android 10; SAMSUNG SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/16.2 Chrome/92.0.4515.166 Mobile Safari/537.36',
         'properties' => [
             'Comment' => 'Samsung Browser 16.2',
