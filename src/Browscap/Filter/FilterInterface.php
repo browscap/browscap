7b8c498159440956cd96000a883d1da9924e0eb4
Tom Chapman <thomas_chapman84@hotmail.com>
2023-01-08 17:54:49 +0000

    Allow FilterCollection to be used anywhere that FilterInterface can be used

--- src/Browscap/Filter/FilterInterface.php
+++ src/Browscap/Filter/FilterInterface.php
@@ -9,10 +9,11 @@ use Browscap\Writer\WriterInterface;
 
 interface FilterInterface
 {
-    public const TYPE_FULL     = 'FULL';
-    public const TYPE_STANDARD = '';
-    public const TYPE_LITE     = 'LITE';
-    public const TYPE_CUSTOM   = 'CUSTOM';
+    public const TYPE_FULL       = 'FULL';
+    public const TYPE_STANDARD   = '';
+    public const TYPE_LITE       = 'LITE';
+    public const TYPE_CUSTOM     = 'CUSTOM';
+    public const TYPE_COLLECTION = 'COLLECTION';
 
     /**
      * returns the Type of the filter
--- tests/BrowscapTest/Filter/FilterCollectionTest.php
+++ tests/BrowscapTest/Filter/FilterCollectionTest.php
@@ -12,6 +12,8 @@ use PHPUnit\Framework\MockObject\MethodCannotBeConfiguredException;
 use PHPUnit\Framework\MockObject\MethodNameAlreadyConfiguredException;
 use PHPUnit\Framework\TestCase;
 
+use function range;
+
 class FilterCollectionTest extends TestCase
 {
     private FilterCollection $object;
@@ -20,6 +22,50 @@ class FilterCollectionTest extends TestCase
     protected function setUp(): void
     {
         $this->object = new FilterCollection();
+    }
+
+    /**
+     * tests getter for the filter type with filters of a different types, and without filters
+     *
+     * @dataProvider dataProviderForTestGetType
+     */
+    public function testGetType(int $noOfFilters): void
+    {
+        for ($i = 0; $i < $noOfFilters; $i++) {
+            $mockFilter = $this->createMock(FilterInterface::class);
+            $mockFilter
+                ->expects(static::never())
+                ->method('getType');
+            $mockFilter
+                ->expects(static::never())
+                ->method('isOutput');
+            $mockFilter
+                ->expects(static::never())
+                ->method('isOutputSection');
+            $mockFilter
+                ->expects(static::never())
+                ->method('isOutputProperty');
+
+            $this->object->addFilter($mockFilter);
+        }
+
+        static::assertSame(FilterInterface::TYPE_COLLECTION, $this->object->getType());
+    }
+
+    /**
+     * Data Provider for testGetType
+     *
+     * @return array<array<int>>
+     */
+    public function dataProviderForTestGetType(): array
+    {
+        $funcArgs = [];
+
+        foreach (range(0, 10) as $noOfFilters) {
+            $funcArgs[] = [$noOfFilters];
+        }
+
+        return $funcArgs;
     }
 
     /**
