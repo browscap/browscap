dfe91ed0959555d52afe8ef14cbb45ec56f9c0b8
Thomas Müller <mimmi20@live.de>
2023-02-27 21:20:39 +0100

    fix tests

--- src/Browscap/Command/Helper/ValidateHelper.php
+++ src/Browscap/Command/Helper/ValidateHelper.php
@@ -50,10 +50,8 @@ class ValidateHelper extends Helper
 
         $schemaDecoded = $schemaStorage->getSchema($schemaUri);
 
-        /** @phpstan-ignore-next-line  */
         assert($schemaDecoded instanceof stdClass || $schemaDecoded === null);
 
-        /** @phpstan-ignore-next-line  */
         if ($schemaDecoded === null) {
             $logger->critical('the given json schema is invalid');
 
