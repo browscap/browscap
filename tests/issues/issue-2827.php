7622d27f6f781463ea062fff1740ac65839633bc
Paul Rutter <paul@blueconic.com>
2023-07-26 10:39:43 +0200

    https://github.com/browscap/browscap/issues/2827 - Add two useragents, Chrome 115 on MacOSX and Chrome 115 on iOS

--- tests/issues/issue-2827.php
+++ tests/issues/issue-2827.php
@@ -31,4 +31,31 @@ return [
         'standard' => true,
         'full' => true,
     ],
+    'issue-2827-C' => [
+        'ua' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+        'properties' => [
+            'Platform' => 'MacOSX',
+            'Platform_Version' => '10',
+            'Platform_Maker' => 'Apple Inc',
+            'Browser_Version' => 'Chrome',
+            'Browser_Version' => '115',
+            'Comment' => 'Chrome 115.0',
+        ],
+        'lite' => false,
+        'standard' => true,
+        'full' => true,
+    ],
+    'issue-2827-D' => [
+        'ua' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/115.0.5790.130 Mobile/15E148 Safari/604.1',
+        'properties' => [
+            'Platform' => 'iOS',
+            'Platform_Maker' => 'Apple Inc',
+            'Browser_Version' => 'Chrome',
+            'Browser_Version' => '115',
+            'Comment' => 'Chrome 115.0',
+        ],
+        'lite' => false,
+        'standard' => true,
+        'full' => true,
+    ],
 ];
